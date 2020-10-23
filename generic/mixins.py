8319b789bee736d208e1a1548b723258ac0bd09f
Serg <ochkarik1983@mail.ru@>
2020-10-23 17:13:43 +0300

    H

--- generic/mixins.py
+++ generic/mixins.py
@@ -8,6 +8,6 @@ class CategoryListMixin(ContextMixin):
 	def get_context_data(self,**kwargs):
 		context = super(CategoryListMixin,self).get_context_data(**kwargs)
 		context["current_url"] = self.request.path
-		context["federal_elect_list"] = AuthorityList.objects.filter(is_reginal=False)
+		context["federal_elect_list"] = AuthorityList.objects.only("pk")
 		context["blog_cat"] = BlogCategory.objects.only("pk")
 		return context

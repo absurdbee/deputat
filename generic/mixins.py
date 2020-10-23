e6e86b07e5c0ea308dfa8f3b4b87b87f5997e86b
Serg <ochkarik1983@mail.ru@>
2020-10-23 17:04:37 +0300

    H

--- generic/mixins.py
+++ generic/mixins.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import ContextMixin
 from django.conf import settings
-from lists.models import ElectList, BlogCategory
+from lists.models import AuthorityList, BlogCategory
 
 
 class CategoryListMixin(ContextMixin):
@@ -8,6 +8,6 @@ class CategoryListMixin(ContextMixin):
 	def get_context_data(self,**kwargs):
 		context = super(CategoryListMixin,self).get_context_data(**kwargs)
 		context["current_url"] = self.request.path
-		context["federal_elect_list"] = ElectList.objects.filter(is_reginal=False)
+		context["federal_elect_list"] = AuthorityList.objects.filter(is_reginal=False)
 		context["blog_cat"] = BlogCategory.objects.only("pk")
 		return context

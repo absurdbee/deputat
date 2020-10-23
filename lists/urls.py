5c41e8d96efc5ad15510e27c5c605f2d2dd050a4
Serg <ochkarik1983@mail.ru@>
2020-10-23 17:05:28 +0300

    H

--- lists/urls.py
+++ lists/urls.py
@@ -5,6 +5,6 @@ from lists.views import *
 urlpatterns = [
     url(r'^elect_list/$', ElectListsView.as_view(), name='elect_list'),
 
-    url(r'^(?P<slug>[\w\-]+)/$', AuthorityList.as_view(), name='authority_index'),
+    url(r'^(?P<slug>[\w\-]+)/$', AuthorityListView.as_view(), name='authority_index'),
     url(r'^(?P<slug>[\w\-]+)/$', FractionList.as_view(), name='fraction_index'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,27 +1,27 @@
 from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
 from django.views.generic import ListView
-from lists.models import ElectList
+from lists.models import AuthorityList
 from elect.models import Elect
 
 
-class AuthorityList(ListView, CategoryListMixin):
+class AuthorityListView(ListView, CategoryListMixin):
 	template_name = "elect_list/authority_list.html"
 	paginate_by = 20
 
 	def get(self,request,*args,**kwargs):
 		if self.kwargs["slug"] == None:
-			self.list = ElectList.objects.first()
+			self.list = AuthorityList.objects.first()
 		else:
-			self.list = ElectList.objects.get(slug=self.kwargs["slug"])
-		return super(AuthorityList,self).get(request,*args,**kwargs)
+			self.list = AuthorityList.objects.get(slug=self.kwargs["slug"])
+		return super(AuthorityListView,self).get(request,*args,**kwargs)
 
 	def get_queryset(self):
 		elects = Elect.objects.filter(list=self.list)
 		return elects
 
 	def get_context_data(self,**kwargs):
-		context = super(AuthorityList,self).get_context_data(**kwargs)
+		context = super(AuthorityListView,self).get_context_data(**kwargs)
 		context["list"] = self.list
 		return context
 

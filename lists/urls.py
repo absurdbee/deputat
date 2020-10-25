2fa59f40d8e639b8b8a99a2ab1827d9a5168802f
Serg <ochkarik1983@mail.ru@>
2020-10-25 14:05:28 +0300

    H

--- lists/urls.py
+++ lists/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^elect_list/$', ElectListsView.as_view(), name='elect_list'),
 
     url(r'^(?P<slug>[\w\-]+)/$', AuthorityListView.as_view(), name='authority_index'),
-    url(r'^(?P<slug>[\w\-]+)/$', FractionList.as_view(), name='fraction_index'),
+    url(r'^fraction/(?P<slug>[\w\-]+)/$', FractionList.as_view(), name='fraction_index'),
+    url(r'^region/(?P<slug>[\w\-]+)/$', RegionElectView.as_view(), name='region_index'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,13 @@
 from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
 from django.views.generic import ListView
-from lists.models import AuthorityList
+from lists.models import *
 from elect.models import Elect
 
 
 class AuthorityListView(ListView, CategoryListMixin):
+	""" Чиновники выбранного списка: госдумы, совфеда и т.д. """
+
 	template_name = "elect_list/authority_list.html"
 	paginate_by = 20
 
@@ -27,6 +29,8 @@ class AuthorityListView(ListView, CategoryListMixin):
 
 
 class FractionList(ListView, CategoryListMixin):
+	""" Чиновники выбранной фракции госдумы """
+
 	template_name = "elect_list/fraction_list.html"
 	paginate_by = 20
 
@@ -40,9 +44,26 @@ class FractionList(ListView, CategoryListMixin):
 		return context
 
 class ElectListsView(TemplateView, CategoryListMixin):
+	""" Списки чиновников: госдума, совфед и подобное """
+
 	template_name = "elect_list/all_list.html"
 
 	def get_context_data(self,**kwargs):
 		context = super(ElectListsView,self).get_context_data(**kwargs)
 		context["lists"] = AuthorityList.objects.only("pk")
+		return context
+
+
+class RegionElectView(TemplateView, CategoryListMixin):
+	""" Чиновники (госдумы, совфеда и т.д.) выбранного региона """
+
+	template_name = "elect_list/authority_list.html"
+
+	def get(self,request,*args,**kwargs):
+		self.region = Region.objects.get(slug=self.kwargs["slug"])
+		return super(RegionElectView,self).get(request,*args,**kwargs)
+
+	def get_context_data(self,**kwargs):
+		context = super(RegionElectView,self).get_context_data(**kwargs)
+		context["region"] = self.region
 		return context

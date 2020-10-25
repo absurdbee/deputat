c0f932d629e547077b79c315e847bd513685eddd
Serg <ochkarik1983@mail.ru@>
2020-10-25 17:17:40 +0300

    H

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
+from lists.models import Region
 
 
-class MainPageView(TemplateView,CategoryListMixin):
+class MainPageView(TemplateView, CategoryListMixin):
 	template_name="main/mainpage.html"
+
+	def get_context_data(self,**kwargs):
+		context = super(MainPageView,self).get_context_data(**kwargs)
+		context["regions"] = Region.objects.only("pk")
+		return context

d26772390db8f7590172a497ff75bc2d22b3fb34
Serg <ochkarik1983@mail.ru@>
2020-10-26 14:18:21 +0300

    H

--- elect/admin.py
+++ elect/admin.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
-from elect.models import Elect, LinkElect
+from elect.models import Elect, LinkElect, EducationElect
+
+
+class LinkElectInline(admin.TabularInline):
+    model = LinkElect
+class EducationElectInline(admin.TabularInline):
+    model = EducationElect
 
 
 class ElectAdmin(admin.ModelAdmin):
+    inlines = [
+        LinkElectInline,
+        EducationElectInline,
+    ]
     list_display = ['name', ]
     list_filter = ['name',]
     search_fields = ['name',]
     class Meta:
             model = Elect
 
-class LinkElectAdmin(admin.ModelAdmin):
-    list_display = ['title','elect']
-    list_filter = ['elect']
-    search_fields = ['title','elect',]
-    class Meta:
-            model = LinkElect
 
 admin.site.register(Elect, ElectAdmin)
-admin.site.register(LinkElect, LinkElectAdmin)

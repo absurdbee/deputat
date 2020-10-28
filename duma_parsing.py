4005e80c0b3de0bda5b8cc1037d7f127ebf51e63
Serg <ochkarik1983@mail.ru@>
2020-10-28 14:54:58 +0300

    J

--- duma_parsing.py
+++ duma_parsing.py
@@ -16,7 +16,7 @@ def get_page_data(html):
         fraction = soup.find('a', class_='person__description__link').text
     except:
         fraction = ''
-    data = {'name': _name.replace("<h1 class='article__title article__title--person'>", "").replace("<br/>", " ").replace("<h1/>", ""),
+    data = {'name': _name.replace('<h1 class="article__title article__title--person">', '').replace('<br/>', ' ').replace('<h1/>', ''),
             'fraction': fraction}
     return data
 

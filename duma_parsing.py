964010033bca5d8b8ec92f83b3d2139a199c4097
Serg <ochkarik1983@mail.ru@>
2020-10-28 11:16:15 +0300

    J

--- duma_parsing.py
+++ duma_parsing.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'http://duma.gov.ru/duma/persons/99112808/'
+url = 'https://habr.com/ru/post/280238/'
 r = requests.get(url)
 with open('test.html', 'w') as output_file:
   output_file.write(r.text)

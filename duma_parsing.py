eb7d83238b7869e4b83e6737b2016780be106669
Serg <ochkarik1983@mail.ru@>
2020-10-28 11:09:04 +0300

    J

--- duma_parsing.py
+++ duma_parsing.py
@@ -4,4 +4,4 @@ import requests
 url = 'http://duma.gov.ru/duma/persons/99112808/'
 r = requests.get(url)
 with open('test.html', 'w') as output_file:
-  output_file.write(r.text.encode('cp1251'))
+  output_file.write(r.text.encode('utf-8'))

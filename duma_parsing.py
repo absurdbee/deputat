9c2ade48587599109e9e221734d21f09b4c54ec8
Serg <ochkarik1983@mail.ru@>
2020-10-28 12:34:23 +0300

    J

--- duma_parsing.py
+++ duma_parsing.py
@@ -2,7 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://habr.com/ru/post/280238/'
-r = requests.get(url)
-with open('test.html', 'w') as output_file:
-  output_file.write(r.text)
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        name = text_before_word(soup.find('div', class_='article__title article__title--person').text)
+    except:
+        name = ''
+    try:
+        fraction = text_before_word(soup.find('div', class_='link link--underline person__description__link').text)
+    except:
+        fraction = ''
+    data = {'name': name,
+            'fraction': fraction}
+    return data
+
+
+def main():
+    start = datetime.now()
+    url = 'http://duma.gov.ru/duma/persons/99112808/'
+    html = get_html(url)
+    data = get_page_data(html)
+    print data
+
+if __name__ == '__main__':
+    main()

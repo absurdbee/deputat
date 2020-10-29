59c8f8e4faea1bffabc5b5d5ecbef2f03e7a2ccc
Serg <ochkarik1983@mail.ru@>
2020-10-29 18:15:41 +0300

    J

--- test.py
+++ test.py
@@ -83,7 +83,7 @@ def get_page_data(html):
 
 
 def main():
-    url = 'http://duma.gov.ru/duma/persons/99100829/'
+    url = 'http://duma.gov.ru/duma/persons/99111836/'
     html = get_html(url)
     data = get_page_data(html)
     print(data)

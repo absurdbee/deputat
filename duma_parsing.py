71563dd5ea01b69ffe15092a4b40195595626a97
Serg <ochkarik1983@mail.ru@>
2020-11-02 13:41:08 +0300

    H

--- duma_parsing.py
+++ duma_parsing.py
@@ -146,7 +146,7 @@ def get_page_data(html):
 
 
 def main():
-    url = 'http://duma.gov.ru/duma/persons/99100142/'
+    url = 'http://duma.gov.ru/duma/persons/99100788/'
     html = get_html(url)
     data = get_page_data(html)
     if not Elect.objects.filter(name=data["name"]).exists():

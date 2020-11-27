c7d14539d2aeb8a5967258cdf4f1457775b33689
Serg <ochkarik1983@mail.ru@>
2020-11-27 09:51:25 +0300

    J

--- blog_cat/urls.py
+++ blog_cat/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from blog_cat.views import BlogListView, BlogLists
+from blog_cat.views import BlogListView
 
 
 urlpatterns = [
-    url(r'^$', BlogLists.as_view(), name='blog_list'),
-    url(r'^(?P<slug>[\w\-]+)/$', BlogListView.as_view(), name='blog_index'),
+    url(r'^$', BlogListView.as_view(), name='blog_list'),
 ]

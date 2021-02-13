30d93ad431873027476f061269fd46b7be3327b4
Serg <ochkarik1983@mail.ru@>
2021-02-13 15:41:50 +0300

    J

--- users/admin.py
+++ users/admin.py
@@ -2,17 +2,11 @@ from django.contrib import admin
 from users.models import User
 from users.model.profile import UserLocation
 
-
-class UserLocationInline(admin.TabularInline):
-    model = UserLocation
-
 class UserAdmin(admin.ModelAdmin):
     list_display = ['pk', 'phone']
-    inlines = [
-        UserLocationInline,
-    ]
     search_fields = ('last_name','first_name')
     model = User
 
 
 admin.site.register(User)
+admin.site.register(UserLocation)

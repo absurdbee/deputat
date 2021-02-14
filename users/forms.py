d09377176eaa01de762a30b8c1f656246917ba71
Serg <ochkarik1983@mail.ru@>
2021-02-14 13:24:49 +0300

    J

--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from users.models import User
 from django import forms
 
-class UserForm(forms.ModelForm):
 
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('avatar',)
+        fields = ('avatar','first_name', 'last_name')

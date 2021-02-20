d99cff203d10b2cb06b510a3c82894c3db480daa
Serg <ochkarik1983@mail.ru@>
2021-02-20 20:18:40 +0300

    J

---
+++ users/view/progs.py
@@ -0,0 +1,63 @@
+from django.views import View
+from users.models import User
+from django.http import HttpResponse
+from django.http import Http404
+from common.template.user import render_for_platform
+
+
+class PhoneVerify(View):
+    def get(self,request,*args,**kwargs):
+        from common.model.other import PhoneCodes
+
+        if not request.is_ajax():
+            raise Http404
+        code = self.kwargs["code"]
+        _phone = self.kwargs["phone"]
+        phone = request.user.get_last_location().phone + _phone
+        try:
+            obj = PhoneCodes.objects.get(phone=phone)
+        except:
+            obj = None
+        if obj:
+            user = User.objects.get(pk=request.user.pk)
+            user.perm = User.STANDART
+            user.phone = obj.phone
+            user.save()
+            obj.delete()
+            data = 'ok'
+            response = render_for_platform(request,'generic/response/phone.html',{'response_text':data})
+            return response
+        else:
+            data = 'Код подтверждения неверный. Проверьте, пожалуйста, номер, с которого мы Вам звонили. Последние 4 цифры этого номера и есть код подтверждения, который нужно ввести с поле "Последние 4 цифры". Если не можете найти номер, нажмите на кнопку "Перезвонить повторно".'
+            response = render_for_platform(request,'generic/response/phone.html',{'response_text':data})
+            return response
+
+
+class PhoneSend(View):
+    def get(self,request,*args,**kwargs):
+        import json, requests
+        from common.model.other import PhoneCodes
+
+        text = ""
+        if not request.is_ajax() and not request.user.is_no_phone_verified():
+            raise Http404
+        else:
+            _phone = self.kwargs["phone"]
+            if len(_phone) > 8:
+                phone = request.user.get_last_location().phone + _phone
+                try:
+                    user = User.objects.get(phone=phone)
+                    data = 'Пользователь с таким номером уже зарегистрирован. Используйте другой номер или напишите в службу поддержки, если этот номер Вы не использовали ранее.'
+                    response = render_for_platform(request,'generic/response/phone.html',{'response_text':data})
+                    return response
+                except:
+                    response = requests.get(url="https://api.ucaller.ru/v1.0/initCall?service_id=12203&key=GhfrKn0XKAmA1oVnyEzOnMI5uBnFN4ck&phone=" + phone)
+                    data = response.json()
+                    PhoneCodes.objects.create(phone=phone, code=data['code'])
+                    data = 'Мы Вам звоним. Последние 4 цифры нашего номера - код подтверждения, который нужно ввести в поле "Последние 4 цифры" и нажать "Подтвердить"'
+                    response = render_for_platform(request,'generic/response/code_send.html',{'response_text':data})
+                    return response
+            else:
+                data = 'Введите, пожалуйста, корректное количество цифр Вашего телефона'
+                response = render_for_platform(request,'generic/response/phone.html',{'response_text':data})
+                return response

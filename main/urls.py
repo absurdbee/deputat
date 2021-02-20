5c00636eb6bd0b82d8918c2753478256f3142c42
Serg <ochkarik1983@mail.ru@>
2021-02-20 18:42:31 +0300

    J

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.views import *
 
 
 urlpatterns = [
-	url(r'^$', MainPageView.as_view(), name="main"),
+	url(r'', MainPageView.as_view(), name="main"),
 	url(r'^phone_verify/$', MainPhoneSend.as_view(), name="phone_send"),
 	url(r'^phone_send/(?P<phone>\d+)/$', PhoneSend.as_view()),
     url(r'^phone_verify/(?P<phone>\d+)/(?P<code>\d+)/$', PhoneVerify.as_view()),

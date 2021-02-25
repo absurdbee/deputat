373f7cd78f850db1f12dfb856dc096fa28f88081
Serg <ochkarik1983@mail.ru@>
2021-02-25 18:20:22 +0300

    J

--- blog/url/comments.py
+++ blog/url/comments.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from blog.comments.views import *
+from blog.view.comments import *
 from django.contrib.auth.decorators import login_required
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -7,11 +7,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', BlogDetailView.as_view(), name="blog_detail"),
     url(r'^news/$', ProectNewsView.as_view(), name="proect_news"),
     url(r'^all_elects_news/(?P<name>[\w\-]+)/$', AllElectsNewsView.as_view(), name="all_elect_news"),
-
-    url(r'^post-comment/$', login_required(BlogCommentCreate.as_view())),
-    url(r'^reply-comment/$', login_required(BlogReplyCreate.as_view())),
-    url(r'^delete_comment/(?P<pk>\d+)/$', login_required(BlogCommentDelete.as_view())),
-    url(r'^abort_delete_comment/(?P<pk>\d+)/$', login_required(BlogCommentAbortDelete.as_view())),
     url(r'^comments/(?P<pk>\d+)/$', BlogCommentList.as_view()),
 
     url(r'^progs/', include('blog.url.progs')),

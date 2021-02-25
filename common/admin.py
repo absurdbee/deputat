53c24b196280df6518117880fc05d9d2d21d3259
Serg <ochkarik1983@mail.ru@>
2021-02-25 19:04:48 +0300

    J

--- common/admin.py
+++ common/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from common.model.other import *
 from common.model.comments import *
+from common.model.votes import *
 
 
 class BlogCommentAdmin(admin.ModelAdmin):

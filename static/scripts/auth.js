3e3aa102b71a7ebc64090520fbc3c7e0e137d747
Serg <ochkarik1983@mail.ru@>
2021-03-02 17:32:45 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -1,5 +1,23 @@
 function on(elSelector,eventName,selector,fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while(el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
-
+function ajax_get_reload(url) {
+  var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+    ajax_link.open( 'GET', url, true );
+    ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    ajax_link.onreadystatechange = function () {
+      if ( this.readyState == 4 && this.status == 200 ) {
+        elem_ = document.createElement('span');
+        elem_.innerHTML = ajax_link.responseText;
+        ajax = elem_.querySelector("#reload_block");
+        rtr = document.getElementById('ajax');
+        rtr.innerHTML = ajax.innerHTML;
+        window.scrollTo(0,0);
+        document.title = elem_.querySelector('title').innerHTML;
+        window.history.pushState({route: url}, "network", url);
+        get_select()
+      }
+    }
+    ajax_link.send();
+}
 
 on('body', 'click', '#register_ajax', function() {
   form = document.querySelector("#signup");

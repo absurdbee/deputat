e260bd6b9a6455be57b89618c86ce5696ca184dc
Serg <ochkarik1983@mail.ru@>
2021-03-20 11:52:09 +0300

    J

--- users/models.py
+++ users/models.py
@@ -94,28 +94,28 @@ class User(AbstractUser):
         return SubscribeElect.objects.filter(user_id=self.pk).values("pk").count()
 
     def get_like_news(self):
-        from common.model.votes import ElectNewVotes
+        from common.model.votes import ElectNewVotes2
         from blog.models import ElectNew
 
-        likes = ElectNewVotes.objects.filter(user_id=self.pk, vote=ElectNewVotes.LIKE).values("new_id")
+        likes = ElectNewVotes2.objects.filter(user_id=self.pk, vote=ElectNewVotes.LIKE).values("new_id")
         news_ids = [new['new_id'] for new in likes]
         return ElectNew.objects.filter(id__in=news_ids)
 
     def get_like_news_count(self):
-        from common.model.votes import ElectNewVotes
-        return ElectNewVotes.objects.filter(user_id=self.pk, vote=ElectNewVotes.LIKE).values("new_id").count()
+        from common.model.votes import ElectNewVotes2
+        return ElectNewVotes2.objects.filter(user_id=self.pk, vote=ElectNewVotes.LIKE).values("new_id").count()
 
     def get_dislike_news(self):
-        from common.model.votes import ElectNewVotes
+        from common.model.votes import ElectNewVotes2
         from blog.models import ElectNew
 
-        dislikes = ElectNewVotes.objects.filter(user_id=self.pk, vote="D").values("new_id")
+        dislikes = ElectNewVotes2.objects.filter(user_id=self.pk, vote="D").values("new_id")
         news_ids = [new['new_id'] for new in dislikes]
         return ElectNew.objects.filter(id__in=news_ids)
 
     def get_dislike_news_count(self):
-        from common.model.votes import ElectNewVotes
-        return ElectNewVotes.objects.filter(user_id=self.pk, vote="D").values("new_id").count()
+        from common.model.votes import ElectNewVotes2
+        return ElectNewVotes2.objects.filter(user_id=self.pk, vote="D").values("new_id").count()
 
     def is_deleted(self):
         return try_except(self.perm == User.DELETED)

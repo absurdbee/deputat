084f8ede554bba7c1f52c4ce17a970d35912f996
Serg <ochkarik1983@mail.ru@>
2021-03-21 18:05:30 +0300

    J

--- deputat/settings.py
+++ deputat/settings.py
@@ -113,7 +113,7 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'deputat_7',
+        'NAME': 'deputat_8',
         'USER': 'serg',
         'PASSWORD': 'ulihos46',
         'HOST': 'localhost',

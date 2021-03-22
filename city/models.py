ca7cf30db11d7c4e7294ecaafc42874755726979
Serg <ochkarik1983@mail.ru@>
2021-03-22 13:35:09 +0300

    J

--- city/models.py
+++ city/models.py
@@ -8,7 +8,7 @@ class City(models.Model):
 	name = models.CharField(max_length=100, verbose_name="Название")
 	slug = AutoSlugField(populate_from='name', unique=True, db_index=True)
 	order = models.PositiveSmallIntegerField(default=0, verbose_name="Порядковый номер")
-	region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="+", blank=True, null=True, verbose_name="Регион")
+	region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="cities_region", blank=True, null=True, verbose_name="Регион")
 
 	def __str__(self):
 		return self.name

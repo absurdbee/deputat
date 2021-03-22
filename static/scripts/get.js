bedf12897ca277d9c15016dbbe3102be66b5b1f8
Serg <ochkarik1983@mail.ru@>
2021-03-22 19:37:57 +0300

    J

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -45,10 +45,10 @@ function loadScripts( src ) {
 }
 
 loadScripts('/static/scripts/functions/general.js');
-loadScripts('/static/scripts/auth.js');
 loadScripts('/static/scripts/functions/comment_attach.js');
 loadScripts('/static/scripts/functions/preview.js');
 loadScripts('/static/scripts/main.js');
 loadScripts('/static/scripts/progs/blog_news.js');
 loadScripts('/static/scripts/progs/elect.js');
 loadScripts('/static/scripts/progs/user.js');
+loadScripts('/static/scripts/auth.js');

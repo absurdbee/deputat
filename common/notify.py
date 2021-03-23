25c682eef08d0a97e255bd4c82ee0dc390a911ad
Serg <ochkarik1983@mail.ru@>
2021-03-23 15:41:46 +0300

    J

--- common/notify.py
+++ common/notify.py
@@ -53,6 +53,6 @@ def get_notify(user, notify):
                 first_notify.creator.get_full_name() + '</a> и ещё ' + str(notify.count_object_set()) + first_notify.get_verb_display()\
                  + ' запись </p>' + get_blog(user, attach[3:])
             else:
-                return '<p style="padding-left: 7px;"><a href="' + notify.creator.get_link() + '" class="ajax" style="font-weight: bold;">'+ \
+                return '<p style="padding-left: 7px;"><a href="/users/' + str(notify.creator.pk) + '" class="ajax" style="font-weight: bold;">'+ \
                 notify.creator.get_full_name() + '</a>' + notify.get_verb_display()\
                  + ' запись </p>' + get_blog(user, attach[3:])

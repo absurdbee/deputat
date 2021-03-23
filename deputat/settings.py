c96703a06ca90ea4a499fea61bf3aadf8b329765
Serg <ochkarik1983@mail.ru@>
2021-03-23 13:51:10 +0300

    J

--- deputat/settings.py
+++ deputat/settings.py
@@ -97,6 +97,7 @@ TEMPLATES = [
                 "django.template.context_processors.media",
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
+                "django.template.context_processors.messages",
             ],
         },
     },

d66a0d061a79c6c754b94bdcee1917d4883c7a47
Serg <ochkarik1983@mail.ru@>
2021-03-23 13:19:29 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -184,3 +184,22 @@ function phone_check() {
       };
       request.send(null)
   });
+  on('#ajax', 'change', '.select_region', function() {
+    var val = this.value;
+    if (val == '') {
+      this.nextElementSibling.innerHTML = "";
+    } else {
+      var link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+      link.open( 'GET', "/region/cat/" + val + "/", true );
+      link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      link.onreadystatechange = function () {
+        if ( link.readyState == 4 ) {
+            if ( link.status == 200 ) {
+                var sub = document.getElementById("subcat");
+                sub.innerHTML = link.responseText;
+            }
+        }
+    };
+    link.send( null );
+    };
+  });

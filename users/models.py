6b57ea3f5273d69d431d3988fcf39db9dc0ab9f2
Serg <ochkarik1983@mail.ru@>
2021-03-23 13:48:21 +0300

    J

--- users/models.py
+++ users/models.py
@@ -32,6 +32,7 @@ class User(AbstractUser):
     s_avatar = models.ImageField(blank=True, upload_to=upload_to_user_directory)
     gender = models.CharField(max_length=5, choices=GENDER, blank=True, verbose_name="Пол")
     device = models.CharField(max_length=5, choices=DEVICE, blank=True, verbose_name="Оборудование")
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name='Город')
     USERNAME_FIELD = 'phone'
 
     class Meta:

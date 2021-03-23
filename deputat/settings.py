f58383c276c143a02f34475e0a5edf5849b0723b
Serg <ochkarik1983@mail.ru@>
2021-03-23 13:50:03 +0300

    J

--- deputat/settings.py
+++ deputat/settings.py
@@ -13,7 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.sites',

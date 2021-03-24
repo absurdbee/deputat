300e692133aa024c9f84df8d6ffd42f97b032c23
Serg <ochkarik1983@mail.ru@>
2021-03-24 16:19:46 +0300

    J

---
+++ delete_model.py
@@ -0,0 +1,16 @@
+import sys,os
+import re
+import requests
+from bs4 import BeautifulSoup
+from bs4.dammit import EncodingDetector
+
+project_dir = '../deputat/deputat/'
+
+sys.path.append(project_dir)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+import django
+django.setup()
+
+from blog.models import Blog
+
+Blog.objects.all()delete()

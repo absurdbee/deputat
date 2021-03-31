7a1d033c6bd8467db575bb768ba5645f4c3eb4f3
Serg <ochkarik1983@mail.ru@>
2021-03-31 15:03:09 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -217,7 +217,7 @@ on('body', 'click', '.menu_nav_2', function() {
 on('body', 'click', '.reply_comment', function() {
   div = this.parentElement.nextElementSibling;
   input = div.querySelector(".text-comment");
-  input.value = this.parentElement.parentElement.querySelector(".first_name") + ', ';
+  input.value = this.parentElement.parentElement.querySelector(".first_name").innerHTML + ', ';
   div.style.display = "block";
   input.focus();
 })

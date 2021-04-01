cb97a4c892c56fa89aa69862979bc52c48c07f3d
Serg <ochkarik1983@mail.ru@>
2021-04-01 16:13:32 +0300

    J

--- users/url/settings.py
+++ users/url/settings.py
@@ -3,8 +3,8 @@ from users.view.settings import *
 
 
 urlpatterns = [
-    url(r'^$', UserProfileSettings.as_view()),
-    url(r'^notify/$', UserNotifySettings.as_view()),
-    url(r'^private/$', UserPrivateSettings.as_view()),
-    url(r'^quard/$', UserQuardSettings.as_view()),
+    url(r'^$', UserProfileSettings.as_view(), name="settings_profile"),
+    url(r'^notify/$', UserNotifySettings.as_view(), name="settings_notify"),
+    url(r'^private/$', UserPrivateSettings.as_view(), name="settings_private"),
+    url(r'^quard/$', UserQuardSettings.as_view(), name="settings_quard"),
 ]

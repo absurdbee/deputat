1621595b69075db7a9ef15981eb068510f0f3b6f
Serg <ochkarik1983@mail.ru@>
2021-04-05 13:36:53 +0300

    J

--- music/admin.py
+++ music/admin.py
@@ -5,10 +5,8 @@ from music.models import *
 class MusicAdmin(admin.ModelAdmin):
     list_display = ['title']
     search_fields = ['title']
-    list_filter = ['tag', 'genre']
     class Meta:
             model = Music
-
 
 admin.site.register(Music, MusicAdmin)
 admin.site.register(SoundList)

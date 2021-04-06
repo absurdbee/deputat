cf3d5aa7be83cba983111e4905a4f77f3f0deeb3
Serg <ochkarik1983@mail.ru@>
2021-04-06 16:35:15 +0300

    J

--- gallery/views.py
+++ gallery/views.py
@@ -53,7 +53,7 @@ class UserAlbum(ListView):
 
 	def get(self,request,*args,**kwargs):
 		self.user = User.objects.get(pk=self.kwargs["pk"])
-		self.album = Album.objects.get(pk=self.kwargs["list_pk"])
+		self.album = Album.objects.get(uuid=self.kwargs["uuid"])
 		if self.user.pk == request.user.pk:
 			self.photo_list = self.album.get_staff_photos()
 		else:

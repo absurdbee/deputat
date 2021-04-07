039fc4c795f63259588cddc4a91a70c94eb5caf4
Serg <ochkarik1983@mail.ru@>
2021-04-07 12:45:23 +0300

    J

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,7 @@ urlpatterns=[
 	url(r'^(?P<pk>\d+)/$', UserGallery.as_view(), name="user_gallery"),
 	url(r'^album/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserAlbum.as_view(), name="user_album"),
 	url(r'^load/(?P<pk>\d+)/$', UserLoadAlbum.as_view()),
-    url(r'^photo/(?P<pk>\d+)/(?P<photo_pk>\d+)/$', UserAlbumPhoto.as_view(), name="u_photo"),
+    url(r'^photo/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserAlbumPhoto.as_view(), name="u_photo"),
 	url(r'^post_photo/(?P<pk>\d+)/(?P<photo_pk>\d+)/$', UserElectNewPhoto.as_view(), name="u_elect_new_photo"),
 	url(r'^comment_photo/(?P<pk>\d+)/(?P<photo_pk>\d+)/$', UserCommentPhoto.as_view(), name="u_elect_new_comment_photo"),
 	url(r'^preview_photo/(?P<pk>\d+)/$', GetUserPhoto.as_view()),

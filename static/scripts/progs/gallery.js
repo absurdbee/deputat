dc127d0743ae7b7fbb05e25b42e4f052adbc03e8
Serg <ochkarik1983@mail.ru@>
2021-04-09 13:00:45 +0300

    J

--- static/scripts/progs/gallery.js
+++ static/scripts/progs/gallery.js
@@ -194,6 +194,6 @@ on('body', 'click', '.u_elect_new_comment_photo', function() {
 on('body', 'click', '.u_load_photo_album', function() {
   parent = this.parentElement.parentElement.parentElement;
   pk = parent.getAttribute("data-pk");
-  loader = document.getElementById("item_loader");
+  loader = document.getElementById("loader");
   open_fullscreen("/gallery/load_album/" + pk + "/", loader)
 });

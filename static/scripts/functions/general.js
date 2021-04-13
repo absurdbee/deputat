158acc5ace0add92b1a73738fdb5a2e068c8456a
Serg <ochkarik1983@mail.ru@>
2021-04-13 12:57:13 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -126,7 +126,7 @@ on('body', 'click', '.photo_attach_album_remove', function() {
   block.remove();
 });
 on('body', 'click', '.doc_attach_list_remove', function() {
-  block = this.parentElement.parentElement.parentElement; 
+  block = this.parentElement.parentElement; 
   if (block.classList.contains("attach_block")){
     remove_file_attach(), is_full_attach()
   } else if (block.parentElement.classList.contains("comment_attach_block")){

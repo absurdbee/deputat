4fbdee220dff89d67c38895fb1ff53294d7b2a5e
Serg <ochkarik1983@mail.ru@>
2021-04-13 18:47:43 +0300

    J

--- music/url/user_progs.py
+++ music/url/user_progs.py
@@ -5,10 +5,11 @@ from music.view.user_progs import *
 urlpatterns = [
     url(r'^create_soundcloud_set/(?P<pk>\d+)/$', UserSoundcloudSetCreate.as_view()),
     url(r'^soundcloud_set/(?P<uuid>[0-9a-f-]+)/$', UserSoundcloudSet.as_view()),
-    url(r'^create_list/(?P<pk>\d+)/$', UserPlaylistCreate.as_view()),
+    url(r'^create_list/$', UserPlaylistCreate.as_view()),
     url(r'^edit_list/(?P<uuid>[0-9a-f-]+)/$', UserPlaylistEdit.as_view()),
     url(r'^delete_list/(?P<uuid>[0-9a-f-]+)/$', UserPlaylistDelete.as_view()),
     url(r'^abort_delete_list/(?P<uuid>[0-9a-f-]+)/$', UserPlaylistAbortDelete.as_view()),
+    
     url(r'^add_list/(?P<uuid>[0-9a-f-]+)/$', UserPlaylistAdd.as_view()),
     url(r'^remove_list/(?P<uuid>[0-9a-f-]+)/$', UserPlaylistRemove.as_view()),
 

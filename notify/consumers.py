5e99ff094dda6a2e4e817fcf1c4c3fe39891379f
Serg <ochkarik1983@mail.ru@>
2021-04-16 14:34:19 +0300

    J

---
+++ notify/consumers.py
@@ -0,0 +1,20 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class NotificationsConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        if self.scope["user"].is_anonymous:
+            await self.close()
+
+        else:
+            await self.channel_layer.group_add(
+                'notification', self.channel_name)
+            await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+           'notification', self.channel_name)
+
+    async def receive(self, text_data):
+        await self.send(text_data=json.dumps(text_data))

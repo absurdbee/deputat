13d7c25fa936cab66a3f7152564a148fc8b1b517
Serg <ochkarik1983@mail.ru@>
2021-04-16 17:49:58 +0300

    J

--- common/items/blog.py
+++ common/items/blog.py
@@ -88,11 +88,11 @@ def get_comment_blog(user, notify):
             + notify.get_verb_display() + ' ' + str(notify.count_user_set_comment()) + ' к новости</p>' + blog(user, _attach[2][3:])
         elif notify.is_have_object_set():
             first_notify = notify.get_first_object_set()
-            return '<p style="padding-left: 7px;"><a href="' + first_notify.creator.get_link() + '" class="ajax" style="font-weight: bold;">'+ \
+            return '<p style="padding-left: 7px;"><a href="/users/' + str(first_notify.creator.pk) + '/" class="ajax" style="font-weight: bold;">'+ \
             first_notify.creator.get_full_name() + '</a> и ещё ' + str(notify.count_object_set()) + first_notify.get_verb_display()\
              + ' новость </p>' + blog(user, _attach[2][3:])
         else:
-            return '<p style="padding-left: 7px;"><a href="' + notify.creator.get_link() + '" class="ajax" style="font-weight: bold;">'+ \
+            return '<p style="padding-left: 7px;"><a href="/users/' + str(notify.creator.pk) + '/" class="ajax" style="font-weight: bold;">'+ \
             notify.creator.get_full_name() + '</a>' + notify.get_verb_display()\
              + ' новость </p>' + blog(user, _attach[2][3:])
     if attach[:3] == "blc":

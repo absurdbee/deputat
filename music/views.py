2b67345e417d8ed877d502b6ca9a87de9bfcf533
Serg <ochkarik1983@mail.ru@>
2021-04-18 17:02:52 +0300

    J

--- music/views.py
+++ music/views.py
@@ -18,34 +18,30 @@ class AllMusicView(TemplateView):
 
 
 class UserMusic(ListView):
-	template_name, paginate_by = None, 15
+    template_name, paginate_by = None, 15
 
-	def get(self,request,*args,**kwargs):
+    def get(self,request,*args,**kwargs):
         pk = self.kwargs["pk"]
-		self.user = User.objects.get(pk=pk)
-		self.list = self.user.get_or_create_main_playlist()
-		if self.user.pk == request.user.pk:
-			self.music_list = self.list.get_my_playlist()
+        self.user = User.objects.get(pk=pk)
+        self.list = self.user.get_or_create_main_playlist()
+        if self.user.pk == request.user.pk:
+            self.music_list = self.list.get_my_playlist()
             self.is_have_lists = self.list.is_have_my_lists(pk)
-			self.get_lists = self.list.get_my_lists(pk)
-		else:
-			self.music_list = self.list.get_playlist()
+            self.get_lists = self.list.get_my_lists(pk)
+        else:
+            self.music_list = self.list.get_playlist()
             self.is_have_lists = self.list.is_have_lists(pk)
-			self.get_lists = self.list.get_lists(pk)
-		self.template_name = get_list_template(self.list, "user_music/main/", "list.html", request.user, request.META['HTTP_USER_AGENT'])
-		return super(UserMusic,self).get(request,*args,**kwargs)
+            self.get_lists = self.list.get_lists(pk)
+        self.template_name = get_list_template(self.list, "user_music/main/", "list.html", request.user, request.META['HTTP_USER_AGENT'])
+        return super(UserMusic,self).get(request,*args,**kwargs)
 
-	def get_context_data(self,**kwargs):
-		context = super(UserMusic,self).get_context_data(**kwargs)
-		context['user'] = self.user
-		context['list'] = self.list
-        context['is_have_lists'] = self.is_have_lists
-		context['get_lists'] = self.get_lists
-		context['count_lists'] = self.count_lists
-		return context
+    def get_context_data(self,**kwargs):
+        c = super(UserMusic,self).get_context_data(**kwargs)
+        c['user'], c['list'], c['is_have_lists'], c['get_lists'], c['count_lists'] = self.user, self.list, self.is_have_lists, self.get_lists, self.count_lists
+        return c
 
-	def get_queryset(self):
-		return self.music_list
+    def get_queryset(self):
+        return self.music_list
 
 
 class UserMusicList(ListView):

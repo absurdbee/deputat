1c53feae6251c6032925252e317fe4cb0349e7de
Serg <ochkarik1983@mail.ru@>
2021-04-18 15:31:15 +0300

    J

--- users/models.py
+++ users/models.py
@@ -420,41 +420,11 @@ class User(AbstractUser):
         albums_query.add(Q(creator_id=self.pk), Q.AND)
         return Album.objects.filter(albums_query)
 
-    def get_my_all_doc_lists(self):
-        from docs.models import DocList
-        docs_query = Q(type="LIS") | Q(type="PRI") | Q(type="MAI")
-        docs_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
-        return DocList.objects.filter(docs_query)
-    def is_have_my_all_doc_lists(self):
-        from docs.models import DocList
-        docs_query = Q(type="LIS") | Q(type="PRI") | Q(type="MAI")
-        docs_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
-        return DocList.objects.filter(docs_query).exists()
     def get_my_doc_lists(self):
         from docs.models import DocList
-        docs_query = Q(type="LIS") | Q(type="PRI")
-        docs_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
+        docs_query = ~Q(type="CLO") | ~Q(type="DEL")
+        docs_query.add(Q(creator_id=self.id), Q.AND)
         return DocList.objects.filter(docs_query)
-    def is_have_my_doc_lists(self):
-        from docs.models import DocList
-        docs_query = Q(type="LIS") | Q(type="PRI")
-        docs_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
-        return DocList.objects.filter(docs_query).exists()
-    def get_doc_lists(self):
-        from docs.models import DocList
-        docs_query = Q(type="LIS")
-        docs_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
-        return DocList.objects.filter(docs_query).order_by("order")
-    def is_have_doc_lists(self):
-        from docs.models import DocList
-        docs_query = Q(type="LIS")
-        docs_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
-        return DocList.objects.filter(docs_query).exists()
-    def count_doc_lists(self):
-        from docs.models import DocList
-        docs_query = Q(type="LIS")
-        docs_query.add(Q(Q(creator_id=self.id)|Q(users__id=self.pk)), Q.AND)
-        return DocList.objects.filter(docs_query).values("pk").count()
 
     def get_my_playlists(self):
         from music.models import SoundList

b1fbef09edac409d34a1717b5b3bc837ea725f49
Serg <ochkarik1983@mail.ru@>
2021-04-20 17:31:54 +0300

    J

--- static/scripts/functions/preview.js
+++ static/scripts/functions/preview.js
@@ -225,6 +225,7 @@ function create_preview_doc(media_body, pk){
 function create_preview_doc_list(name, pk, count){
   $div = document.createElement("div");
   $div.classList.add("col-md-6", "col-sm-12", "folder");
+  $div.style.textAlign = "center";
   $div.setAttribute("doclist-pk", pk);
 
   $input = document.createElement("span");

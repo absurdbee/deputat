72f0658d9df64c3ddd57899f2e6449575b4c1356
Serg <ochkarik1983@mail.ru@>
2021-04-20 15:42:38 +0300

    J

--- common/model/comments.py
+++ common/model/comments.py
@@ -2,10 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.postgres.indexes import BrinIndex
 from blog.models import Blog, ElectNew
-from elect.models import Elect
-from users.helpers import upload_to_user_directory
-from pilkit.processors import ResizeToFill, ResizeToFit, Transpose
-from imagekit.models import ProcessedImageField
+from django.db.models import Q
 
 
 """

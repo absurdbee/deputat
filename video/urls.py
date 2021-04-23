b2c132b8e4a26eb0c8954859b4df803aeaae57f4
Serg <ochkarik1983@mail.ru@>
2021-04-23 17:48:56 +0300

    J

--- video/urls.py
+++ video/urls.py
@@ -7,10 +7,6 @@ urlpatterns = [
     url(r'^load/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserLoadVideoAlbum.as_view()),
     url(r'^user_video/(?P<pk>\d+)/$', UserVideo.as_view(), name='user_video'),
     url(r'^user_videolist/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserVideoList.as_view(), name='user_videolist'),
-    url(r'^create_video_attach/(?P<pk>\d+)/$', UserVideoAttachCreate.as_view()),
-    url(r'^create_video/(?P<pk>\d+)/$', UserVideoCreate.as_view()),
-    url(r'^create_list/(?P<pk>\d+)/$', UserVideoListCreate.as_view()),
-    url(r'^edit_list/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserVideolistEdit.as_view()),
 
     url(r'^user_progs/', include('video.url.user_progs')),
 ]

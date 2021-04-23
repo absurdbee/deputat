b64a2ce9f80812484e59bea7330754dbefc5a93d
Serg <ochkarik1983@mail.ru@>
2021-04-23 18:23:51 +0300

    J

---
+++ static/scripts/progs/video.js
@@ -0,0 +1,265 @@
+on('body', 'click', '.u_video_list_add', function() {
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/video/user_progs/add_list/", loader)
+});
+on('body', 'click', '.u_video_add', function() {
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/video/user_progs/create_video/", loader)
+});
+on('body', 'click', '.u_video_edit', function() {
+  parent = this.parentElement.parentElement.parentElement;
+  blocks = document.body.querySelectorAll('.col-sm-12');
+  for (var i = 0; i < blocks.length; i++) {blocks[i].classList.remove("edited_video")}
+
+  parent.parentElement.parentElement.parentElement.classList.add("edited_video")
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/video/user_progs/edit_video/" + parent.getAttribute("data-pk") +"/", loader)
+});
+on('body', 'click', '.u_video_list_edit', function() {
+  list = document.body.querySelectorAll('.cover_block');
+  for (var i = 0; i < list.length; i++) {
+    list[i].classList.remove("list_active")
+  }
+  block = this.parentElement.parentElement.parentElement.parentElement;
+  block.classList.add("list_active");
+  uuid = block.getAttribute('data-uuid');
+  loader = document.getElementById("create_loader");
+  open_fullscreen("/video/user_progs/edit_list/" + uuid + "/", loader)
+});
+on('body', 'click', '.u_video_list_remove', function() {
+  block = this.parentElement.parentElement.parentElement.parentElement;
+  this.parentElement.parentElement.style.display = "none";
+
+  uuid = block.getAttribute('data-uuid');
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link_.open( 'GET', "/video/user_progs/delete_list/" + uuid + "/", true );
+  link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    a = block.querySelector(".content-wrapper");
+    e = a.querySelector(".file-name");
+    e.classList.add("u_video_list_abort_remove", "pointer");
+    e.innerHTML = "Восстановить";
+    a.nextElementSibling.innerHTML = "Удалённый"
+  }}
+  link_.send();
+});
+on('body', 'click', '.u_video_list_abort_remove', function() {
+  _this = this;
+  block = this.parentElement.parentElement.parentElement.parentElement;
+  block.querySelector(".dropdown").style.display = "block";
+  uuid = block.getAttribute('data-uuid');
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link_.open( 'GET', "/video/user_progs/abort_delete_list/" + uuid + "/", true );
+  link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    _this.classList.remove("u_video_list_abort_remove", "pointer");
+    _this.innerHTML = _this.getAttribute("data-name");
+    _this.parentElement.nextElementSibling.innerHTML = "Приватный"
+  }}
+  link_.send();
+});
+
+on('body', 'click', '#u_create_video_list_btn', function() {
+  form = this.parentElement.parentElement.parentElement;
+  form_data = new FormData(form);
+  if (!form.querySelector("#id_name").value){
+    form.querySelector("#id_name").style.border = "1px #FF0000 solid";
+    toast_error("Название - обязательное поле!");
+  } else { this.disabled = true }
+  post_and_load_object_page(form, "/video/user_progs/add_list/", "/video/list/", "/");
+});
+
+on('body', 'click', '#u_edit_video_list_btn', function() {
+  form = this.parentElement.parentElement.parentElement;
+  form_data = new FormData(form);
+  if (!form.querySelector("#id_name").value){
+    form.querySelector("#id_name").style.border = "1px #FF0000 solid";
+    toast_error("Название - обязательное поле!");
+  } else { this.disabled = true }
+  uuid = form.getAttribute("data-uuid")
+
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link_.open( 'POST', "/video/user_progs/edit_list/" + uuid + "/", true );
+  link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    close_create_window();
+    title = form.querySelector('#id_name').value;
+
+    album = document.body.querySelector(".list_active");
+    album.querySelector(".videoname").innerHTML = title;
+    album.classList.remove("album_active");
+    toast_success("Видеоальбом изменен")
+  }}
+  link_.send(form_data);
+});
+
+on('body', 'click', '.u_add_video_in_list', function() {
+  _this = this;
+  parent = _this.parentElement;
+  uuid = parent.getAttribute("data-uuid");
+  pk = parent.parentElement.parentElement.parentElement.getAttribute("data-pk");
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', '/video/user_progs/add_video_in_list/' + pk + "/" + uuid + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    list = parent.querySelector(".u_add_video_in_list");
+    list.style.paddingLeft = "14px";
+    list.classList.add("u_remove_video_in_list");
+    list.classList.remove("u_add_video_in_list");
+    span = document.createElement("span");
+    span.innerHTML = '<svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg> ';
+    list.prepend(span)
+  }};
+  link.send( null );
+})
+on('body', 'click', '.u_remove_video_in_list', function() {
+  _this = this;
+  parent = _this.parentElement;
+  uuid = parent.getAttribute("data-uuid");
+  pk = parent.parentElement.parentElement.parentElement.getAttribute("data-pk");
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', '/docs/user_progs/remove_video_in_list/' + pk + "/" + uuid + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    list = parent.querySelector(".u_remove_video_in_list");
+    list.style.paddingLeft = "30px";
+    list.classList.add("u_add_video_in_list");
+    list.classList.remove("u_remove_video_in_list");
+    list.querySelector("svg").remove();
+  }};
+  link.send( null );
+})
+
+on('body', 'click', '#u_create_video_btn', function() {
+  _this = this;
+  form = _this.parentElement.parentElement.parentElement;
+  form_data = new FormData(form);
+
+  lists = form.querySelector("#id_list");
+  selectedOptions = lists.selectedOptions;
+  val = false;
+  for (var i = 0; i < selectedOptions.length; i++) {
+    if(selectedOptions[i].value) {val = true}
+  }
+
+  if (!form.querySelector("#id_title").value){
+    form.querySelector("#id_title").style.border = "1px #FF0000 solid";
+    toast_error("Название - обязательное поле!")
+  } else if (!val){
+    form.querySelector("#id_list").style.border = "1px #FF0000 solid";
+    toast_error("Выберите список!")
+  }
+  else if (!form.querySelector("#id_file").value){
+    form.querySelector("#id_file").style.border = "1px #FF0000 solid";
+    toast_error("Загрузите видеозапись!")
+  } else { _this.disabled = true }
+
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link_.open( 'POST', "/video/user_progs/create_video/", true );
+  link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    elem = link_.responseText;
+    response = document.createElement("span");
+    response.innerHTML = elem;
+    document.body.querySelector(".pk_saver").getAttribute("data-uuid") ? (
+      uuid = document.body.querySelector(".pk_saver").getAttribute("data-uuid"),
+      check_span1(response.querySelector('.span1'), uuid, response.innerHTML),
+      document.body.querySelector(".video_empty") ? document.body.querySelector(".video_empty").style.display = "none" : null) : get_preview(response, "video");
+    toast_info("Видеозапись создана!")
+    close_create_window();
+  }};
+
+  link_.send(form_data);
+});
+
+on('body', 'click', '#u_edit_video_btn', function() {
+  form = this.parentElement.parentElement.parentElement;
+  pk = form.getAttribute("data-pk");
+  form_data = new FormData(form);
+
+  lists = form.querySelector("#id_list");
+  selectedOptions = lists.selectedOptions;
+  val = false;
+  for (var i = 0; i < selectedOptions.length; i++) {
+    if(selectedOptions[i].value) {val = true}
+  }
+
+  if (!form.querySelector("#id_title").value){
+    form.querySelector("#id_title").style.border = "1px #FF0000 solid";
+    toast_error("Название - обязательное поле!")
+  } else if (!val){
+    form.querySelector("#id_list").style.border = "1px #FF0000 solid";
+    toast_error("Выберите список!")
+  }
+  else if (!form.querySelector("#id_file").value){
+    form.querySelector("#id_file").style.border = "1px #FF0000 solid";
+    toast_error("Загрузите видеозапись!")
+  } else { this.disabled = true }
+
+  link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link_.open( 'POST', "/video/user_progs/edit_video/" + pk + "/", true );
+  link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+  link_.onreadystatechange = function () {
+  if ( this.readyState == 4 && this.status == 200 ) {
+    toast_info("Видеозапись изменена!")
+    close_create_window();
+    elem = link_.responseText;
+    response = document.createElement("span");
+    response.innerHTML = elem;
+    video = document.body.querySelector(".edited_video");
+    video.innerHTML = response.querySelector(".pag").innerHTML;
+  }};
+
+  link_.send(form_data);
+});
+
+on('body', 'click', '.u_video_remove', function() {
+  saver = this.parentElement.parentElement.parentElement;
+  pk = saver.getAttribute("data-pk")
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', "/video/user_progs/remove_video/" + pk + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    div = document.createElement("div");
+    div.classList.add("col-md-6", "col-sm-12");
+    div.style.padding = "20px";
+    div.style.display =  "block";
+    div.innerHTML = "Видеоапись удалена. <span class='u_videoabort_remove pointer underline' data-pk='" + pk + "'>Восстановить</span>";
+    item = saver.parentElement.parentElement.parentElement;
+    item.style.display = "none"; item.parentElement.insertBefore(div, item)
+  }};
+  link.send( );
+});
+on('body', 'click', '.u_video_abort_remove', function() {
+  pk = this.getAttribute("data-pk");
+  block = this.parentElement; next = block.nextElementSibling;
+  link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+  link.open( 'GET', "/video/user_progs/abort_remove_video/" + pk + "/", true );
+  link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+  link.onreadystatechange = function () {
+  if ( link.readyState == 4 && link.status == 200 ) {
+    block.remove();
+    next.style.display = "block";
+  }};
+  link.send();
+});
+
+on('body', 'click', '.u_load_video_list', function() {
+  parent = this.parentElement.parentElement.parentElement;
+  pk = parent.getAttribute("videolist-pk");
+  loader = document.getElementById("window_loader");
+  open_fullscreen("/video/load/" + pk + "/", loader)
+});

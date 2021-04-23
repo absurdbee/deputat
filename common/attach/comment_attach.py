8f08e303d3b1e8994401217d5336a44deba6590a
Serg <ochkarik1983@mail.ru@>
2021-04-23 16:46:37 +0300

    J

--- common/attach/comment_attach.py
+++ common/attach/comment_attach.py
@@ -24,10 +24,6 @@ def get_u_blog_comment_attach(comment, user):
             try:
                 from music.models import Music
                 music = Music.objects.get(query, pk=item[3:])
-                if music.artwork_url:
-                    figure = ''.join(['<a class="music_list_comment music_thumb pointer"><img style="width:30px;heigth:auto" src="', music.artwork_url.url, '" alt="img" /></a>'])
-                else:
-                    figure = '<a class="music_list_comment music_thumb pointer"><svg width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-play"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg></a>'
                 span_btn, lists = '', ''
                 if user.is_authenticated:
                     if user.pk == music.creator.pk:
@@ -36,14 +32,14 @@ def get_u_blog_comment_attach(comment, user):
                         options = '<a class="dropdown-item track_manager_remove pointer">♦ Удалить</a>'
                     else:
                         options = '<span class="dropdown-item track_claim">Пожаловаться</span>'
-                    opt_drop = '<div class="dropdown" style="position: inherit;"><a class="btn_default drop pointer"><svg style="width: 17px;padding-bottom: 2px;" fill="currentColor" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg></a><div class="dropdown-menu dropdown-menu-right" style="top: 35px;">' + options + '<span class="dropdown-item copy_link">Копировать ссылку</span></div></div>'
+                    opt_drop = '<div class="dropdown" style="position: inherit;"><a class="btn_default drop pointer"><svg style="width: 17px;padding-bottom: 2px;" fill="currentColor" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg></a><div class="dropdown-menu dropdown-menu-right" style="top: 60px;">' + options + '<span class="dropdown-item copy_link">Копировать ссылку</span></div></div>'
                     for list in user.get_my_playlists():
                         if list.is_item_in_list(music.pk):
                             lists = ''.join([lists, '<span data-uuid="', str(list.uuid), '"><span class="dropdown-item u_remove_track_in_list"><svg fill="currentColor" style="width:15px;height:15px;" class="svg_default" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>', list.name, '</span></span>'])
                         else:
                             lists = ''.join([lists, '<span data-uuid="', str(list.uuid), '"><span class="dropdown-item u_add_track_in_list" style="padding-left: 30px;">', list.name, '</span></span>'])
-                    span_btn = ''.join([span_btn, '<span class="span_btn" style="margin-left:auto;display:flex" data-pk="', str(music.pk), '"><span class="dropdown" style="position: inherit;"><span class="btn_default pointer drop"><svg fill="currentColor" style="width:25px;height:25px;" class="svg_default" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg></span><div class="dropdown-menu dropdown-menu-right" style="top: 35px;">', lists, '</div></span>', opt_drop, '</span>'])
-                block = ''.join([block, '<div class="col-md-6 col-sm-12" data-path="', music.get_uri(), '" data-duration="', str(music.duration), '" style="flex-basis: 100%;"><div class="media border" music-counter="0">', figure, '<div class="media-body" style="display: flex;"><h6 class="music_list_comment" style="padding-top: 5px;"><a>', music.title, '</a></h6>', span_btn, '</div></div></div>'])
+                    span_btn = ''.join([span_btn, '<span class="span_btn" data-pk="', str(music.pk), '"><span class="dropdown" style="position: inherit;"><span class="btn_default pointer drop"><svg fill="currentColor" style="width:25px;height:25px;" class="svg_default" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg></span><div class="dropdown-menu dropdown-menu-right" style="top: 60px;">', lists, '</div></span>', opt_drop, '</span>'])
+                block = ''.join([block, '<div class="col-md-6 col-sm-12" style="flex-basis: 100%;"><div class="media border p-1"><div class="media-body doc_media_body" style="line-height: 8px;"><span>', music.title, '</span><div class="audio_div"><audio id="player" class="audio_player"><source src="{{ object.file.url }}" type="audio/mp3" /></audio></div>', span_btn, '</div></div></div>'])
             except:
                 pass
         elif item[:3] == "doc":

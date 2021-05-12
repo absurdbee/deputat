15ffba63b6d62822bf35e77310e7edfeb63d4dd1
Serg <ochkarik1983@mail.ru@>
2021-05-12 17:44:52 +0300

    J

--- common/templates.py
+++ common/templates.py
@@ -25,7 +25,7 @@ def get_full_template(template, request_user, user_agent):
     if request_user.is_authenticated:
         if request_user.is_no_phone_verified():
             template_name = "generic/phone_verification.html"
-        elif request_user.is_blocked():
+        elif request_user.is_closed():
             template_name = "generic/you_global_block.html"
         elif request_user.is_manager():
             template_name = template
@@ -44,7 +44,7 @@ def get_small_template(template, request_user, user_agent):
     if request_user.is_authenticated:
         if request_user.is_no_phone_verified():
             template_name = "generic/phone_verification.html"
-        elif request_user.is_blocked():
+        elif request_user.is_closed():
             template_name = "generic/you_global_block.html"
         else:
             template_name = template
@@ -87,14 +87,14 @@ def get_list_template(list, folder, template, request_user, user_agent):
         elif user.pk == request_user.pk:
             if user.is_suspended():
                 template_name = "generic/you_suspended.html"
-            elif user.is_blocked():
+            elif user.is_closed():
                 template_name = "generic/you_global_block.html"
             else:
                 template_name = folder + "my_" + template
         elif request_user.pk != user.pk:
             if user.is_suspended():
                 template_name = "generic/user_suspended.html"
-            elif user.is_blocked():
+            elif user.is_closed():
                 template_name = "generic/user_global_block.html"
             elif request_user.is_manager() or request_user.is_supermanager():
                 template_name = folder + "staff_" + template
@@ -107,7 +107,7 @@ def get_list_template(list, folder, template, request_user, user_agent):
     elif request_user.is_anonymous:
         if user.is_suspended():
             template_name = "generic/anon_user_suspended.html"
-        elif user.is_blocked():
+        elif user.is_closed():
             template_name = "generic/anon_user_global_block.html"
         elif list.type == "PRI":
             template_name = folder + "anon_private_" + template
@@ -127,14 +127,14 @@ def get_item_template(item, folder, template, request_user, user_agent):
         elif user.pk == request_user.pk:
             if user.is_suspended():
                 template_name = "generic/you_suspended.html"
-            elif user.is_blocked():
+            elif user.is_closed():
                 template_name = "generic/you_global_block.html"
             else:
                 template_name = folder + "my_" + template
         elif request_user.pk != user.pk:
             if user.is_suspended():
                 template_name = "generic/user_suspended.html"
-            elif user.is_blocked():
+            elif user.is_closed():
                 template_name = "generic/user_global_block.html"
             elif request_user.is_manager() or request_user.is_supermanager():
                 template_name = folder + "staff_" + template
@@ -147,7 +147,7 @@ def get_item_template(item, folder, template, request_user, user_agent):
     elif request_user.is_anonymous:
         if user.is_suspended():
             template_name = "generic/anon_user_suspended.html"
-        elif user.is_blocked():
+        elif user.is_closed():
             template_name = "generic/anon_user_global_block.html"
         elif item.status == "PRI":
             template_name = folder + "anon_private_" + template

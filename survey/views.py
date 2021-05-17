43aaf9ab5238c955fb333cd6a1d12281cbac9de8
Serg <ochkarik1983@mail.ru@>
2021-05-17 12:22:51 +0300

    J

--- survey/views.py
+++ survey/views.py
@@ -19,13 +19,9 @@ class UserSurvey(ListView):
 		pk = self.kwargs["pk"]
 		self.user = User.objects.get(pk=pk)
 		self.list = self.user.get_survey_list()
-		self.count_lists = self.list.get_lists_count(pk)
-		if self.user.pk == request.user.pk:
-			self.is_have_lists = self.list.is_have_my_lists(pk)
-			self.get_lists = self.list.get_my_lists(pk)
-		else:
-			self.is_have_lists = self.list.is_have_lists(pk)
-			self.get_lists = self.list.get_lists(pk)
+		self.count_lists = self.list.get_user_lists_count(pk)
+		self.is_have_lists = self.list.is_have_user_lists(pk)
+		self.get_lists = self.list.get_user_lists(pk)
 		if request.user.is_authenticated:
 			self.template_name = get_template_user_item(self.list, "user_survey/main/", "list.html", request.user, request.META['HTTP_USER_AGENT'], request.user.is_survey_manager())
 		else:

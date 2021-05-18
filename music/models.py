ac3dada87b663accf9b691c7700b4b7996039b7b
Serg <ochkarik1983@mail.ru@>
2021-05-18 15:19:14 +0300

    J

--- music/models.py
+++ music/models.py
@@ -187,12 +187,12 @@ class SoundList(models.Model):
     @classmethod
     def get_user_staff_lists(cls, user_pk):
         query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(~Q(type__contains="_"), Q.AND)
+        query.add(~Q(Q(type__contains="_")|Q(type="MAI")), Q.AND)
         return cls.objects.filter(query)
     @classmethod
     def get_user_lists(cls, user_pk):
         query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(Q(Q(type="MAI")|Q(type="LIS")), Q.AND)
+        query.add(Q(type="LIS"), Q.AND)
         return cls.objects.filter(query).order_by("order")
     @classmethod
     def get_user_lists_count(cls, user_pk):
@@ -203,12 +203,12 @@ class SoundList(models.Model):
     @classmethod
     def get_community_staff_lists(cls, community_pk):
         query = Q(community_id=user_pk)|Q(communities__id=community_pk)
-        query.add(~Q(type__contains="_"), Q.AND)
+        query.add(~Q(Q(type__contains="_")|Q(type="MAI")), Q.AND)
         return cls.objects.filter(query)
     @classmethod
     def get_community_lists(cls, community_pk):
         query = Q(community_id=user_pk)|Q(communities__id=community_pk)
-        query.add(Q(Q(type="MAI")|Q(type="LIS")), Q.AND)
+        query.add(Q(type="LIS"), Q.AND)
         return cls.objects.filter(query).order_by("order")
     @classmethod
     def get_community_lists_count(cls, community_pk):

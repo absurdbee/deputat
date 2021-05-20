ccd7ecbdd8adb631bb592a44050c30d01df98e83
Serg <ochkarik1983@mail.ru@>
2021-05-20 17:48:08 +0300

    J

--- static/scripts/progs/doc.js
+++ static/scripts/progs/doc.js
@@ -182,7 +182,7 @@ on('body', 'click', '.u_load_doc_list', function() {
 });
 
 on('body', 'click', '.u_load_profile_doc_list', function() {
-  profile_list_block_load(this, ".load_block", "/doc/user_list/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "u_load_profile_doc_list");
+  profile_list_block_load(this, ".load_block", "/docs/user_list/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "u_load_profile_doc_list");
 });
 
 on('body', 'click', '.u_load_attach_doc_list', function() {

6b23a20a0b5d8c84cefc150a52811d150d471e3d
Serg <ochkarik1983@mail.ru@>
2021-05-20 12:04:55 +0300

    J

--- users/view/load.py
+++ users/view/load.py
@@ -7,7 +7,7 @@ class UserLoadPhoto(ListView):
 
 	def get(self,request,*args,**kwargs):
 		self.list, self.template_name = request.user.get_photo_list(), get_my_template("user_load/u_photo_load.html", request.user, request.META['HTTP_USER_AGENT'])
-		self.get_lists = self.list.get_lists(request.user.pk)
+		self.get_lists = self.list.get_user_lists(request.user.pk)
 		return super(UserLoadPhoto,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
@@ -39,7 +39,7 @@ class UserLoadPhotoComment(ListView):
 
 	def get(self,request,*args,**kwargs):
 		self.list, self.template_name = request.user.get_photo_list(), get_my_template("user_load/u_photo_comments_load.html", request.user, request.META['HTTP_USER_AGENT'])
-		self.get_lists = self.list.get_lists(request.user.pk)
+		self.get_lists = self.list.get_user_lists(request.user.pk)
 		return super(UserLoadPhotoComment,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
@@ -106,7 +106,7 @@ class UserLoadMusic(ListView):
 
 	def get(self,request,*args,**kwargs):
 		self.playlist, self.template_name = request.user.get_playlist(), get_my_template("user_load/u_music_load.html", request.user, request.META['HTTP_USER_AGENT'])
-		self.get_lists = self.playlist.get_lists(request.user.pk)
+		self.get_lists = self.playlist.get_user_lists(request.user.pk)
 		return super(UserLoadMusic,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):
@@ -140,7 +140,7 @@ class UserLoadDoc(ListView):
 
 	def get(self,request,*args,**kwargs):
 		self.list, self.template_name = request.user.get_doc_list(), get_my_template("user_load/u_doc_load.html", request.user, request.META['HTTP_USER_AGENT'])
-		self.get_lists = self.list.get_lists(request.user.pk)
+		self.get_lists = self.list.get_user_lists(request.user.pk)
 		return super(UserLoadDoc,self).get(request,*args,**kwargs)
 
 	def get_context_data(self,**kwargs):

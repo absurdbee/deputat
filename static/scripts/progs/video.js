f896ff584a6f19804e74eed0df33d51983dd19a6
Serg <ochkarik1983@mail.ru@>
2021-05-21 18:14:52 +0300

    J

--- static/scripts/progs/video.js
+++ static/scripts/progs/video.js
@@ -175,7 +175,7 @@ on('body', 'click', '.u_load_video_list', function() {
 });
 
 on('body', 'click', '.u_load_profile_video_list', function() {
-  profile_list_block_load(this, "/video/user_list/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "u_load_profile_video_list");
+  profile_list_block_load(this, ".load_block", "/video/user_list/" + this.parentElement.parentElement.parentElement.getAttribute("data-uuid") + "/", "u_load_profile_video_list");
 });
 
 on('body', 'click', '.u_load_attach_video_list', function() {

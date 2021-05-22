3caf2e153c90383368327b1b778e0f701db6e530
Serg <ochkarik1983@mail.ru@>
2021-05-22 14:05:27 +0300

    J

--- video/url/user_progs.py
+++ video/url/user_progs.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^add_list/$', UserVideolistCreate.as_view()),
     url(r'^edit_list/(?P<uuid>[0-9a-f-]+)/$', UserVideolistEdit.as_view()),
     url(r'^delete_list/(?P<uuid>[0-9a-f-]+)/$', UserVideolistDelete.as_view()),
-    url(r'^abort_delete_list/(?P<uuid>[0-9a-f-]+)/$', AddVideoListFromUserCollections.as_view()),
+    url(r'^abort_delete_list/(?P<uuid>[0-9a-f-]+)/$', AddVideoListInUserCollections.as_view()),
     url(r'^add_list_in_collections/(?P<pk>\d+)/$', RemoveVideoListFromUserCollections.as_view()),
     url(r'^remove_list_from_collections/(?P<pk>\d+)/$', UserVideolistRemove.as_view()),
 

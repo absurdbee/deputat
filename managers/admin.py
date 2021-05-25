d79ede422c44545123c6f7567eccb3acb1f05780
Serg <ochkarik1983@mail.ru@>
2021-05-25 16:43:11 +0300

    J

---
+++ managers/admin.py
@@ -0,0 +1,26 @@
+from managers.models import *
+from django.contrib import admin
+
+
+admin.site.register(UserStaff)
+admin.site.register(CommunityStaff)
+admin.site.register(ElectNewUserStaff)
+admin.site.register(SurveyUserStaff)
+admin.site.register(DocUserStaff)
+admin.site.register(PhotoUserStaff)
+admin.site.register(VideoUserStaff)
+admin.site.register(AudioUserStaff)
+admin.site.register(CanWorkStaffUser)
+
+admin.site.register(CanWorkStaffCommunity)
+admin.site.register(CanWorkStaffElectNewUser)
+admin.site.register(CanWorkStaffSurveyUser)
+admin.site.register(CanWorkStaffDocUser)
+admin.site.register(CanWorkStaffPhotoUser)
+admin.site.register(CanWorkStaffVideoUser)
+admin.site.register(CanWorkStaffAudioUser)
+admin.site.register(ModerationCategory)
+admin.site.register(Moderated)
+
+admin.site.register(ModerationReport)
+admin.site.register(ModerationPenalty)

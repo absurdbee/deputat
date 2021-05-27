a819ec6893fb902e2313d4d381093a08a5298b7a
Serg <ochkarik1983@mail.ru@>
2021-05-27 16:45:43 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -82,8 +82,7 @@ function send_item_sanction(_this, form, url, old_class, new_class, toast) {
     document.getElementById("window_loader").innerHTML="";
 
     if (document.body.querySelector(".load_block")){
-      _this.innerHTML = "Отменить";
-      _this.classList.replace(old_class, new_class)
+      document.body.querySelector(".load_block").innerHTML = '<div class="card mt-3 centered"Ю<div class="card-body" style="margin-top: 10%;"><svg fill="currentColor" class="thumb_big svg_default" viewBox="0 0 24 24"><g><rect fill="none" height="24" width="24"/></g><g><path d="M14,8c0-2.21-1.79-4-4-4S6,5.79,6,8s1.79,4,4,4S14,10.21,14,8z M17,10v2h6v-2H17z M2,18v2h16v-2c0-2.66-5.33-4-8-4 S2,15.34,2,18z"/></g></svg></div><h6>Сущность блокирована.</h6></div>'
     }
     else if (div && div.classList.contains("changed")){
       div.remove();

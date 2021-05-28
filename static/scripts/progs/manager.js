af68581d914f54b69a780e6f46770fe3d1e9a79f
Serg <ochkarik1983@mail.ru@>
2021-05-28 11:27:42 +0300

    J

--- static/scripts/progs/manager.js
+++ static/scripts/progs/manager.js
@@ -77,6 +77,18 @@ on('body', 'click', '.create_user_rejected', function() {
 on('body', 'click', '.u_close_photo_list', function() {
   get_item_sanction_window(this, "", "/managers/progs_photo/list_create_close/")
 })
+on('body', 'click', '.u_close_playlist', function() {
+  get_item_sanction_window(this, "", "/managers/progs_audio/list_create_close/")
+})
+on('body', 'click', '.u_close_video_list', function() {
+  get_item_sanction_window(this, "", "/managers/progs_video/list_create_close/")
+})
+on('body', 'click', '.u_close_survey_list', function() {
+  get_item_sanction_window(this, "", "/managers/progs_survey/list_create_close/")
+})
+on('body', 'click', '.u_close_doc_list', function() {
+  get_item_sanction_window(this, "", "/managers/progs_doc/list_create_close/")
+})
 
 on('body', 'click', '#create_photo_List_close_btn', function() {
   send_item_sanction(this, this.parentElement.parentElement.parentElement, "/managers/progs_photo/list_create_close/", "u_unclose_photo_list", "u_close_photo_list", "Фотоальбом закрыт")

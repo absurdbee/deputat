22b0c944ba680e42feb143debbac4eb32e6ccf4e
Serg <ochkarik1983@mail.ru@>
2021-05-28 12:52:30 +0300

    J

--- managers/models.py
+++ managers/models.py
@@ -719,7 +719,7 @@ class ModerationPenalty(models.Model):
             return ''
     def get_survey_list(self):
         #try:
-        from music.models import SurveyList
+        from survey.models import SurveyList
         list = SurveyList.objects.get(pk=self.object_id)
         creator = list.creator
         image = '<svg fill="currentColor" class="svg_default" style="width:70px;height:70px;" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15 6H3v2h12V6zm0 4H3v2h12v-2zM3 16h8v-2H3v2zM17 6v8.18c-.31-.11-.65-.18-1-.18-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3V8h3V6h-5z"/></svg>'

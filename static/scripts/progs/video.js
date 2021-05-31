764353f9e40e402c199fbd1ba46ec7abb9bbd12c
Serg <ochkarik1983@mail.ru@>
2021-05-31 15:21:36 +0300

    J

--- static/scripts/progs/video.js
+++ static/scripts/progs/video.js
@@ -117,8 +117,8 @@ on('body', 'click', '#u_create_video_btn', function() {
     elem = link_.responseText;
     response = document.createElement("span");
     response.innerHTML = elem;
-    document.body.querySelector(".pk_saver").getAttribute("data-uuid") ? (
-      uuid = document.body.querySelector(".pk_saver").getAttribute("data-uuid"),
+    document.body.querySelector(".uuid_saver").getAttribute("data-uuid") ? (
+      uuid = document.body.querySelector(".uuid_saver").getAttribute("data-uuid"),
       console.log(uuid),
       console.log(response.querySelector('.span1')),
       check_span1(response.querySelector('.span1'), uuid, response.innerHTML),

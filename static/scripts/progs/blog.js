875e6f911c34ab9403ca0709f232bf77f1b95401
Serg <ochkarik1983@mail.ru@>
2021-06-08 16:22:11 +0300

    J

--- static/scripts/progs/blog.js
+++ static/scripts/progs/blog.js
@@ -133,7 +133,7 @@ on('body', 'click', '.blog_comment_like', function() {
 
 on('body', 'click', '.edit_blog_comment', function() {
   _this = this;
-  _this.style.display = "none";
+  //_this.style.display = "none";
   link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
   link.open( 'GET', "/blog/edit_blog_comment/" + _this.parentElement.parentElement.getAttribute("data-pk") + "/", true );
   link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');

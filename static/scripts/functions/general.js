a83c0140d197a9df8d7cf3a85c8af90d3335c14a
Serg <ochkarik1983@mail.ru@>
2021-06-08 14:13:26 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -848,6 +848,8 @@ function send_comment(form, block, link, prepend) {
     link_.open('POST', link, true);
     link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     if (!form.querySelector(".text-comment").value || !form.querySelector(".comment_attach_block").firstChild){
+      console.log(form.querySelector(".text-comment"));
+      console.log(!form.querySelector(".comment_attach_block").firstChild);
       toast_error("Напишите или прикрепите что-нибудь");
       return
     };

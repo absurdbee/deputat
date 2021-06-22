e690e7e4c2cbf1a9f4b0aed8d340cdacbb0cee09
Serg <ochkarik1983@mail.ru@>
2021-06-22 17:42:38 +0300

    J

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ from users.model.settings import *
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email', 's_avatar')
 
 class UserPasswordForm(forms.ModelForm):
 	class Meta:

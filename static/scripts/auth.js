2c3088fb2ecd902f9bfd2be786fd9db5ef448487
Serg <ochkarik1983@mail.ru@>
2021-06-23 14:23:32 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -139,8 +139,8 @@ on('body', 'click', '#logg', function() {
 
     window.location.href = "/"
   } else{
-    response = JSON.parse(link.responseText)
-    document.body.querySelector(".api_response").innerHTML = link.responseJSON;
+    //response = JSON.parse(link.responseText)
+    document.body.querySelector(".api_response").innerHTML = link.responseText[0];
     this.disabled = false
   }};
   link.send(form_data);

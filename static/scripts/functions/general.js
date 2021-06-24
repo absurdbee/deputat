1f26d35876d27d70edc23e5377bc5ae9ea928685
Serg <ochkarik1983@mail.ru@>
2021-06-24 12:50:42 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -863,7 +863,7 @@ function ajax_get_reload(url, prev_url) {
         rtr.innerHTML = ajax.innerHTML;
         window.scrollTo(0,0);
         document.title = elem_.querySelector('title').innerHTML;
-        window.history.pushState("", "", url);
+        window.history.pushState("", elem_.querySelector("title"), url);
         get_select();
         page = 2;
         loaded = false;

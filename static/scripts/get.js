9fcba2fc1cf522c50179765f5e12ac05a4127896
Serg <ochkarik1983@mail.ru@>
2021-06-24 14:26:10 +0300

    J

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -1,13 +1,10 @@
 function on(elSelector,eventName,selector,fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while(el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
 
 
-function get_popstate() {
   window.onpopstate = function( e ) {
   history.back()
   }
-};
 
-get_popstate();
 
 function loadScripts( src ) {
     var script = document.createElement("SCRIPT"),

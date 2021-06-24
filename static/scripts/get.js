4779876e4045d5892b4e0e7adede019ff531d296
Serg <ochkarik1983@mail.ru@>
2021-06-24 13:13:48 +0300

    J

--- static/scripts/get.js
+++ static/scripts/get.js
@@ -4,7 +4,7 @@ function on(elSelector,eventName,selector,fn) {var element = document.querySelec
 function get_popstate() {
   window.addEventListener('popstate', function (e) {
     if (e.state !== null) {
-      history.back()
+      history.back(); return
     }
   })
 };

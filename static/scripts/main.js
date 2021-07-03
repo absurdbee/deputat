027b1a51090069efb72c54332d8956bc65843b5e
Serg <ochkarik1983@mail.ru@>
2021-07-03 16:38:13 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,4 +1,16 @@
 
+function show_hide_password(target){
+	var input = target.previousElementSibling;
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
+
 on('body', 'keydown', '.form-control', function(e) {
   if (e.keyCode == 13) {
     e.preventDefault();

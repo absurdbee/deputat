81e5108d1bd1cb01646739a781022b6fa52bbb41
Serg <ochkarik1983@mail.ru@>
2021-07-04 12:11:40 +0300

    J

--- video/urls.py
+++ video/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^penalty_load/(?P<pk>\d+)/$', UserLoadPenaltyVideoList.as_view()),
     url(r'^moderated_load/(?P<pk>\d+)/$', UserLoadModeratedVideoList.as_view()),
     url(r'^user_video/(?P<pk>\d+)/$', UserVideo.as_view(), name='user_video'),
-    url(r'^user/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserVideoDetail.as_view(), name='u_video'),
+    url(r'^user_detail/(?P<pk>\d+)/(?P<uuid>[0-9a-f-]+)/$', UserVideoDetail.as_view(), name='u_video'),
     url(r'^user_list/(?P<uuid>[0-9a-f-]+)/$', UserVideoList.as_view(), name='user_videolist'),
     url(r'^video_detail/(?P<pk>\d+)/$', UserVideoDetail.as_view()),
 

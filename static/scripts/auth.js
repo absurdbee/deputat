e2edb44fd6aeb4eedb51a51a504e26a023dc46eb
Serg <ochkarik1983@mail.ru@>
2021-07-04 16:11:29 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -133,7 +133,9 @@ on('body', 'click', '#register_ajax', function() {
   if ( reg_link.readyState == 4 && reg_link.status == 201 ) {
     window.location.href = "/users/phone_verify/"
   } else {
-		console.log(reg_link.responseText)
+		if (reg_link.responseText.indexOf( "Введённый пароль" ) != -1) {
+		console.log("Введённый пароль слишком широко распространён.")
+	}
 	}};
   reg_link.send(form_data);
 })

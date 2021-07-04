146e04f7906ff352cd0de5a2ce8b891a4e603a08
Serg <ochkarik1983@mail.ru@>
2021-07-04 16:37:14 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -137,7 +137,10 @@ on('body', 'click', '#register_ajax', function() {
   if ( reg_link.readyState == 4 && reg_link.status == 201 ) {
     window.location.href = "/users/phone_verify/"
   } else {
-		if (reg_link.responseText.indexOf( "Введённый пароль" ) != -1 && !document.body.classList.contains("password")) {
+		if (reg_link.responseText.indexOf("Детям регистрация не разрешена!") !== -1) {
+      toast_error("Портал для пользователей 18+");
+    }
+		else if (reg_link.responseText.indexOf( "Введённый пароль" ) != -1 && !document.body.classList.contains("password")) {
 			alert("Пороль должен состоять минимум из 8 символов - из букв, цифр.");
 			document.body.classList.add("password");
 	} else {

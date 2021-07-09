667dc9f64c1982fddf614a6824f5ebf9bdd23b46
Serg <ochkarik1983@mail.ru@>
2021-07-09 18:44:29 +0300

    J

--- music/helpers.py
+++ music/helpers.py
@@ -18,12 +18,12 @@ def _upload_to_user_directory(creator, filename):
 
 def validate_file_extension(value):
     import os
-    from rest_framework.exceptions import ValidationError
+    from django.http import HttpResponse
     from django.conf import settings
 
     ext = os.path.splitext(value.name)[1]
     valid_extensions = ['.ogg','.mp3','.wav']
     if not ext in valid_extensions:
-        raise ValidationError('Допустимы форматы: ogg, mp3, wav!')
+        return HttpResponse('Допустимы форматы: ogg, mp3, wav!')
     if value.size > settings.MUSIC_FILE_MAX_SIZE:
-        raise ValidationError('Размер не более 5 МБ!')
+        return HttpResponse('Размер не более 5 МБ!')

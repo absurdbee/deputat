85113abbc53f64eae708d6267fee9887a03d004b
Serg <ochkarik1983@mail.ru@>
2021-07-09 11:57:58 +0300

    J

--- video/models.py
+++ video/models.py
@@ -260,7 +260,7 @@ class VideoList(models.Model):
     @classmethod
     def get_user_lists_not_empty(cls, user_pk):
         query = Q(creator_id=user_pk, community__isnull=True)|Q(users__id=user_pk)
-        query.add(~Q(video_list__isnull=True, video_list__type__contains="_"), Q.AND)
+        query.add(~Q(Q(video_list__isnull=True)&Q(video_list__type__contains="_")), Q.AND)
         return cls.objects.filter(query)
 
     @classmethod

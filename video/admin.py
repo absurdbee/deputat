83dae8ca60797de9e0ad9d1ccc73ffe0c2ae1b38
Serg <ochkarik1983@mail.ru@>
2021-07-09 11:05:10 +0300

    J

--- video/admin.py
+++ video/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from video.models import VideoCategory, VideoList, Video
 
 
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type', 'community', 'creator']
+    search_fields = ('title',)
 
 admin.site.register(VideoList)
 admin.site.register(VideoCategory)
-admin.site.register(Video)
+admin.site.register(Video, VideoAdmin)

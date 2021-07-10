c70f4dfda85293cc89de97cae70754e6ca558a28
Serg <ochkarik1983@mail.ru@>
2021-07-10 14:18:03 +0300

    J

--- static/scripts/progs/elect.js
+++ static/scripts/progs/elect.js
@@ -3,6 +3,9 @@
 on('body', 'click', '.u_suggested_elect_new_create', function() {
   loader = document.getElementById("window_loader");
   open_fullscreen("/blog/progs/suggest_elect_new/", loader)
+  if (this.getAttribute("data-name")) {
+    document.body.querySelector(".field_elect_new").innerHTML = this.getAttribute("data-name")
+  }
 });
 
 on('body', 'click', '#u_create_suggested_new_btn', function() {
@@ -25,7 +28,7 @@ on('body', 'click', '#u_create_suggested_new_btn', function() {
   link_.onreadystatechange = function () {
   if ( this.readyState == 4 && this.status == 200 ) {
     toast_info("Новость предложена!")
-    close_default_window(); 
+    close_default_window();
   }};
 
   link_.send(form_data);

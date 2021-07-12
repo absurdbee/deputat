5f3aaa40525ed0a80b35b26d447ce62845a8c5b4
Serg <ochkarik1983@mail.ru@>
2021-07-12 15:58:41 +0300

    J

--- managers/models.py
+++ managers/models.py
@@ -465,7 +465,7 @@ class Moderated(models.Model):
             from blog.models import Blog
             blog = Blog.objects.get(pk=self.object_id)
             creator = blog.creator
-            return ''.join(['<div class="d-flex justify-content-start align-items-center mb-1"><div class="avatar mr-1"><a href="/blog/', str(blog.slug), '/" class="ajax"><img src="', blog.get_image(), '" alt="avatar img" height="40" width="40"></a></div><div class="profile-user-info"><a href="/blog/', str(blog.slug), '/" class="ajax"><h4 class="mb-0">', blog.title, ' <span class="text-muted small">(blog.get_created())</span></h4></a><span class="small">',blog.get_type_display(),'</span></div></div><p class="card-text mb-50">', blog.description, '</p><span class="small" data-pk="', str(blog.pk), '"><span class="create_blog_close pointer underline">Заблокировать</span>&nbsp;&nbsp;<span class="create_blog_rejected pointer underline">Отклонить</span></span>'])
+            return ''.join(['<div class="d-flex justify-content-start align-items-center mb-1"><div class="avatar mr-1"><a href="/blog/', str(blog.slug), '/" class="ajax"><img src="', blog.get_image(), '" alt="avatar img" height="40" width="40"></a></div><div class="profile-user-info"><a href="/blog/', str(blog.slug), '/" class="ajax"><h4 class="mb-0">', blog.title, ' <span class="text-muted small">(blog.get_created())</span></h4></a><span class="small">',blog.get_type_display(),'</span></div></div><p class="card-text mb-50">', blog.description, '</p><span><span><span><span class="small" data-pk="', str(blog.pk), '"><span class="create_blog_close pointer underline">Заблокировать</span>&nbsp;&nbsp;<span class="create_blog_rejected pointer underline">Отклонить</span></span></span></span></span>'])
         except:
             return ''
     def get_elect_new(self, user):
@@ -485,21 +485,21 @@ class Moderated(models.Model):
                 _attach = comment.get_u_attach(user)
             else:
                 _attach = ''
-            return ''.join(['<div class="card-body" style="padding: .5rem .5rem;"><div class="media"><div class="avatar mr-75"><a href="/users/', str(creator.pk), '/" class="ajax"><img src="', creator.get_avatar(), '" width="38" height="38" alt="Avatar"></a></div><div class="media-body"><h6 class="font-weight-bolder mb-25"><a href="/users/', str(creator.pk), '/" class="ajax">', creator.get_full_name(), '</a></h6><span class="text-muted small">', comment.get_created(), '</span><br></div></div><div class="comment_footer"><span class="card-text">', comment.text, '</span>', _attach, ' <div class="border mt-1 btn_console" data-pk="', str(comment.pk), '"><a " obj-pk="', str(self.pk), '" class="show_object_reports pointer">', self.reports_count_ru(), '</a> | <a class="create_blog_comment_close pointer">Закрыть</a> | <a class="create_blog_comment_rejected pointer">Отклонить</a></div></div></div>'])
+            return ''.join(['<div class="card-body" style="padding: .5rem .5rem;"><div class="media"><div class="avatar mr-75"><a href="/users/', str(creator.pk), '/" class="ajax"><img src="', creator.get_avatar(), '" width="38" height="38" alt="Avatar"></a></div><div class="media-body"><h6 class="font-weight-bolder mb-25"><a href="/users/', str(creator.pk), '/" class="ajax">', creator.get_full_name(), '</a></h6><span class="text-muted small">', comment.get_created(), '</span><br></div></div><div class="comment_footer"><span class="card-text">', comment.text, '</span>', _attach, ' <span><div class="border mt-1 btn_console" data-pk="', str(comment.pk), '"><a " obj-pk="', str(self.pk), '" class="show_object_reports pointer">', self.reports_count_ru(), '</a> | <a class="create_blog_comment_close pointer">Закрыть</a> | <a class="create_blog_comment_rejected pointer">Отклонить</a></div></span></div></div>'])
         except:
             return ''
     def get_elect_new_comment(self, user):
-        try:
-            from common.model.comments import ElectNewComment
-            comment = ElectNewComment.objects.get(pk=self.object_id)
-            creator = comment.commenter
-            if comment.attach:
-                _attach = comment.get_u_attach(user)
-            else:
-                _attach = ''
-            return ''.join(['<div class="card-body" style="padding: .5rem .5rem;"><div class="media"><div class="avatar mr-75"><a href="/users/', str(creator.pk), '/" class="ajax"><img src="', creator.get_avatar(), '" width="38" height="38" alt="Avatar"></a></div><div class="media-body"><h6 class="font-weight-bolder mb-25"><a href="/users/', str(creator.pk), '/" class="ajax">', creator.get_full_name(), '</a></h6><span class="text-muted small">', comment.get_created(), '</span><br></div></div><div class="comment_footer"><span class="card-text">', comment.text, '</span>', _attach, ' <span><div class="border mt-1 btn_console" data-pk="', str(comment.pk), '"><a " obj-pk="', str(self.pk), '" class="show_object_reports pointer">', self.reports_count_ru(), '</a> | <a class="create_elect_new_comment_close pointer">Закрыть</a> | <a class="create_elect_new_comment_rejected pointer">Отклонить</a></div></span></div></div>'])
-        except:
-            return ''
+        #try:
+        from common.model.comments import ElectNewComment
+        comment = ElectNewComment.objects.get(pk=self.object_id)
+        creator = comment.commenter
+        if comment.attach:
+            _attach = comment.get_u_attach(user)
+        else:
+            _attach = ''
+        return ''.join(['<div class="card-body" style="padding: .5rem .5rem;"><div class="media"><div class="avatar mr-75"><a href="/users/', str(creator.pk), '/" class="ajax"><img src="', creator.get_avatar(), '" width="38" height="38" alt="Avatar"></a></div><div class="media-body"><h6 class="font-weight-bolder mb-25"><a href="/users/', str(creator.pk), '/" class="ajax">', creator.get_full_name(), '</a></h6><span class="text-muted small">', comment.get_created(), '</span><br></div></div><div class="comment_footer"><span class="card-text">', comment.text, '</span>', _attach, ' <span><div class="border mt-1 btn_console" data-pk="', str(comment.pk), '"><a " obj-pk="', str(self.pk), '" class="show_object_reports pointer">', self.reports_count_ru(), '</a> | <a class="create_elect_new_comment_close pointer">Закрыть</a> | <a class="create_elect_new_comment_rejected pointer">Отклонить</a></div></span></div></div>'])
+        #except:
+        #    return ''
 
     def get_photo(self):
         try:

4dd493e8e32e065f3076139ed27d6f4940e957b8
Serg <ochkarik1983@mail.ru@>
2021-07-13 19:39:03 +0300

    J

--- static/scripts/progs/user.js
+++ static/scripts/progs/user.js
@@ -105,7 +105,7 @@ on('body', 'click', '.remove_elect_subscribe', function() {
         if ( link.status == 200 ) {
           _this.classList.add("add_elect_subscribe"); _this.classList.remove("remove_elect_subscribe");
           _this.innerHTML = '<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart align-middle mr-25"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg><span class="text-truncate">Подписаться</span>'
-          toast_info(name + " удален из подписок");
+          toast_info("Подписка удалена");
         }
     }
 };

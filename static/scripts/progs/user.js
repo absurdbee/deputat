4eccaf14d9033f9818f1c74ef743d2476a97a863
Serg <ochkarik1983@mail.ru@>
2021-07-13 19:34:55 +0300

    J

--- static/scripts/progs/user.js
+++ static/scripts/progs/user.js
@@ -122,7 +122,7 @@ on('body', 'click', '.add_elect_subscribe', function() {
   link.onreadystatechange = function () {
     if ( link.readyState == 4 ) {
         if ( link.status == 200 ) {
-          _this.classList.add("remove_elect_subscribe"); _this.classList.remove("add_elect_subscribe", "btn-primary");
+          _this.classList.add("remove_elect_subscribe"); _this.classList.remove("add_elect_subscribe");
           _this.innerHTML = '<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x align-middle mr-25"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg><span class="text-truncate">Вы подписаны</span>'
           toast_info("Подписка оформлена");
         }

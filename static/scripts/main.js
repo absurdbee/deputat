b3b4ad2805ed51f7c1fee3c497d6fe6e681e7870
Serg <ochkarik1983@mail.ru@>
2021-07-14 16:50:59 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -99,7 +99,7 @@ on('body', 'mouseover', '.map_selector', function(e) {
 	_this = this;
 	popup = _this.parentElement.nextElementSibling;
 	iconPos = _this.getBoundingClientRect();
-  popup.style.left = (e.clientX - 100) + "px";
+  popup.style.left = (e.clientX + 100) + "px";
   popup.style.top = (e.clientY - 350) + "px";
 	popup.querySelector("h3").innerHTML = _this.getAttribute("data-name");
 	popup.style.display = "block";

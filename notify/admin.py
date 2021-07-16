e6d677b089d43c1293171ce2188f8653cd8b9dc1
Serg <ochkarik1983@mail.ru@>
2021-07-16 18:17:16 +0300

    J

--- notify/admin.py
+++ notify/admin.py
@@ -8,8 +8,13 @@ class WallAdmin(admin.ModelAdmin):
     class Meta:
             model = Wall
 
+class NotifyAdmin(admin.ModelAdmin):
+    list_display = ['creator','recipient','verb','type','object_id']
+    search_fields = ['title']
+    class Meta:
+            model = Notify
 
-admin.site.register(Notify)
+admin.site.register(Notify, NotifyAdmin)
 admin.site.register(Wall, WallAdmin)
 
 admin.site.register(UserNewsNotify)

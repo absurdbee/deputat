014a341db283958824223c9d00719db4f08b55d3
Serg <ochkarik1983@mail.ru@>
2021-07-17 17:44:43 +0300

    J

--- users/url/progs.py
+++ users/url/progs.py
@@ -4,7 +4,7 @@ from users.view.progs import *
 
 urlpatterns = [
     url(r'^phone_send/(?P<phone>\d+)/$', PhoneSend.as_view()),
-    url(r'^phone_verify/(?P<phone>\d+)/$', PhoneVerify.as_view()),
+    url(r'^phone_verify/(?P<phone>\d+)/(?P<code>\d+)/$', PhoneVerify.as_view()),
     url(r'^change_phone_send/(?P<phone>\d+)/$', ChangePhoneSend.as_view()),
     url(r'^change_phone_verify/(?P<phone>\d+)/(?P<code>\d+)/$', ChangePhoneVerify.as_view()),
 ]

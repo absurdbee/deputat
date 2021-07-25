a15cc353bdd4caf581adeee8bd8172b7a77621a0
Serg <ochkarik1983@mail.ru@>
2021-07-25 16:16:28 +0300

    J

--- blog/url/progs.py
+++ blog/url/progs.py
@@ -4,7 +4,7 @@ from blog.view.progs import *
 
 urlpatterns = [
     url(r'^add_blog/$', BlogCreateView.as_view()),
-    url(r'^edit_blog/$', BlogEditView.as_view()),
+    url(r'^edit_blog/(?P<pk>\d+)/$', BlogEditView.as_view()),
     url(r'^suggest_elect_new/$', SuggestElectNew.as_view()),
     url(r'^edit_elect_new/(?P<pk>\d+)/$', EditElectNew.as_view()),
     url(r'^edit_manager_elect_new/(?P<pk>\d+)/$', EditManagerElectNew.as_view()),

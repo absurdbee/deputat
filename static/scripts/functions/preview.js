6918fe9ef36e59b968b66c9d35a4540a95a5c2d5
Serg <ochkarik1983@mail.ru@>
2021-07-26 18:56:42 +0300

    J

--- static/scripts/functions/preview.js
+++ static/scripts/functions/preview.js
@@ -106,14 +106,15 @@ function doc_preview_delete(){
 
 function create_preview_photo(img_src, photo_pk, uuid){
   $div = document.createElement("div");
-  $div.classList.add("row_item");
+  $div.classList.add("u_photo_detail");
+  $div.setAttribute('photo-pk', photo_pk);
+  $div.setAttribute('data-uuid', uuid);
+  
   $input = document.createElement("span");
   $input.innerHTML = '<input type="hidden" name="attach_items" value="pho' + photo_pk + '">';
   $img = document.createElement("img");
-  $img.classList.add("u_photo_detail", "image_fit", "pointer");
+  $img.classList.add("image_fit", "pointer");
   $img.setAttribute("src", img_src);
-  $img.setAttribute('photo-pk', photo_pk);
-  $img.setAttribute('data-uuid', uuid);
   $div.append(photo_preview_delete());
   $div.append($input);
   $div.append($img);

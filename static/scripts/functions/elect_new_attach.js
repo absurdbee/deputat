a93944f9df8a0c02dfec0b96b59fb25ed391fcfa
Serg <ochkarik1983@mail.ru@>
2021-07-26 19:59:12 +0300

    J

--- static/scripts/functions/elect_new_attach.js
+++ static/scripts/functions/elect_new_attach.js
@@ -56,7 +56,7 @@ function photo_post_upload_attach(photo_list, block){
   is_full_attach();
   for (var i = 0; i < photo_list.length; i++){
     parent = photo_list[i];
-    div = create_preview_photo(parent.querySelector(".progressive").getAttribute('data-href'), parent.getAttribute("photo-pk"));
+    div = create_preview_photo(parent.querySelector(".progressive").getAttribute('data-href'), parent.getAttribute("photo-pk"), parent.getAttribute("list-uuid"));
     add_file_attach();
     block.append(div);
     is_full_attach();

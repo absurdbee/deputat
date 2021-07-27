bbdcb17468a50ba754a85f7a39f0fd1441240bbf
Serg <ochkarik1983@mail.ru@>
2021-07-27 13:49:03 +0300

    J

--- static/scripts/progs/blog.js
+++ static/scripts/progs/blog.js
@@ -480,6 +480,6 @@ on('body', 'click', '.blog_share_link', function() {
   link_.onreadystatechange = function () {
   if ( this.readyState == 4 && this.status == 200 ) {
     window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;
-  }
+  }}
   link_.send(form_data);
 })

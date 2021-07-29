2400ba1c2aa43e4586a79fe4a15077006dbab1da
Serg <ochkarik1983@mail.ru@>
2021-07-29 15:59:11 +0300

    J

--- quan/urls.py
+++ quan/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from quan.views import QuanView, QuanCategoryView
+from quan.views import *
 
 
 urlpatterns = [
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^how_to_apply/$', HowApplyView.as_view()),
     url(r'^where_to_apply/$', WhereApplyView.as_view()),
     url(r'^why_publish/$', WhyPublishView.as_view()),
-    url(r'^how_to_publish/$', HowPublishView.as_view()), 
+    url(r'^how_to_publish/$', HowPublishView.as_view()),
 ]

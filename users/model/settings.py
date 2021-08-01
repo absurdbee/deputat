32572d43d2ff3f4b991f11539a365a90f13a2370
Serg <ochkarik1983@mail.ru@>
2021-08-01 20:05:18 +0300

    J

--- users/model/settings.py
+++ users/model/settings.py
@@ -55,6 +55,7 @@ class DeputatSend(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", verbose_name="Пользователь", on_delete=models.CASCADE)
     text = models.TextField(max_length=1000, blank=True, verbose_name="Описание спообов идентификации")
 
+    @classmethod
     def create_item(cls, user, text):
         item = cls.objects.create(user=user, text=text)
         return item

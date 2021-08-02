225253c50174fe3dca2a2ebce73476858d907f9f
Serg <ochkarik1983@mail.ru@>
2021-08-02 12:31:47 +0300

    J

--- region/admin.py
+++ region/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from region.models import Region
 
 
-admin.site.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'order']
+    search_fields = ['title']
+    class Meta:
+            model = Region
+
+admin.site.register(Region, RegionAdmin)

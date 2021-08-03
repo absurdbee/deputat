963c30617bb0caee279d19d66b784a537815c70e
Serg <ochkarik1983@mail.ru@>
2021-08-03 19:13:01 +0300

    J

--- city/urls.py
+++ city/urls.py
@@ -4,6 +4,6 @@ from city.views import *
 
 urlpatterns = [
     url(r'^list/(?P<slug>[\w\-]+)/$', CityListView.as_view(), name="city_list"),
+    url(r'^load_city_flat_list/$', LoadCityFlatList.as_view()),
     url(r'^(?P<slug>[\w\-]+)/$', CityDetailView.as_view(), name="city"),
-    url(r'^load_city_flat_list/$', LoadCityFlatList.as_view()), 
 ]

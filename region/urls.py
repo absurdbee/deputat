b03a5004d136921d494985b8f4e177751fffb640
Serg <ochkarik1983@mail.ru@>
2021-08-03 17:00:35 +0300

    J

--- region/urls.py
+++ region/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^region/(?P<slug>[\w\-]+)/$', RegionElectView.as_view(), name='region_index'),
     url(r'^area/(?P<slug>[\w\-]+)/$', RegionDetailView.as_view(), name='region_detail'),
     url(r'^cities/(?P<pk>\d+)/$', LoadCitiesView.as_view()),
+    url(r'^load_regions_dropdown/$', LoadRegionsDropdown.as_view()),
 ]

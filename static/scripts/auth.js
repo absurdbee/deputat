7e7438174fea3fd1fcb4d0baf33932b53a0e0981
Serg <ochkarik1983@mail.ru@>
2021-08-25 11:00:11 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -299,7 +299,7 @@ on('body', 'click', '.phone_send', function() {
     if (val == '') {
       return
     } else {
-			parent = _this.parentElement.parentElement;
+			parent = _this.parentElement.parentElement.parentElement;
 			if (parent.classList.contains("municipal_authorities")) {
 				url = '/city/elects/'
 			}

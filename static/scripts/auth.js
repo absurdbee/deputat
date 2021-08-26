ac770a3e0ee1ebbc762905bc0e4a61a448f4468e
Serg <ochkarik1983@mail.ru@>
2021-08-26 18:49:32 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -315,7 +315,8 @@ on('body', 'click', '.phone_send', function() {
 	on('body', 'click', '.left_menu_select', function() {
 		if (this.value) {
 			this.value = "";
-			this.focus()
+			this.focus();
+			this.focus();
 		} else {
 			return
 		}

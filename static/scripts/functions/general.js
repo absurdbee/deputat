d70b9fd42b3b703e3dd73f969fcc98473e1a9a38
Serg <ochkarik1983@mail.ru@>
2021-08-26 13:02:38 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -208,7 +208,7 @@ function list_load(block, link) {
   request.onreadystatechange = function () {
     if ( request.readyState == 4 && request.status == 200 ) {
       block.innerHTML = request.responseText;
-      block.classList.contains("left_menu_select") ? (block.click(), console.log("click!!")) : null
+      block.classList.contains("form-control") ? (block.click(), console.log("click!!")) : null
     }
   };
   request.send( null );

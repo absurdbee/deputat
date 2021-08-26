cf4380cf18e15fb6961020d26b331d03627f8843
Serg <ochkarik1983@mail.ru@>
2021-08-26 18:30:57 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -27,6 +27,7 @@ on('body', 'click', '.load_left_menu_regions', function(e) {
 	e.preventDefault();
   list_load(this.parentElement.parentElement, "/region/load_left_menu_regions/");
 	document.body.querySelector("#left_menu_regions_2").focus();
+	document.body.querySelector("#left_menu_regions_2").click();
 })
 on('body', 'click', '.load_left_menu_dropdown_regions', function(e) {
 	e.preventDefault();

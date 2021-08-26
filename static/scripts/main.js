2a5d9092fac110aea60a50772163a82187edaf11
Serg <ochkarik1983@mail.ru@>
2021-08-26 12:18:10 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -26,8 +26,7 @@ on('body', 'click', '.show_parent_next_element', function() {
 on('body', 'click', '.load_left_menu_regions', function(e) {
 	e.preventDefault();
   list_load(this.parentElement.parentElement, "/region/load_left_menu_regions/");
-
-	//this.remove()
+	this.parentElement.parentElement.querySelector(".left_menu_select").click()
 })
 on('body', 'click', '.load_left_menu_dropdown_regions', function(e) {
 	e.preventDefault();

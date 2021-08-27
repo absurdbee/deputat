f8fbc9a6c6c75e224b1685adc25bd3192086ad47
Serg <ochkarik1983@mail.ru@>
2021-08-27 21:45:23 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -32,7 +32,7 @@ on('body', 'click', '.load_left_menu_dropdown_regions', function(e) {
   list_load(this.parentElement.parentElement, "/region/load_left_menu_regions_select/");
 });
 
-on('body', 'change', '#left_menu_regions', function() {
+on('body', 'change', '.left_menu_regions', function() {
 	_this = this;
 	val = _this.value;
 	option = _this.nextElementSibling.querySelector('[value=' + '"' + val + '"' + ']')

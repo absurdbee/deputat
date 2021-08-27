5976d9a6f8151476ac9852128456f86a620c8187
Serg <ochkarik1983@mail.ru@>
2021-08-27 17:48:10 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -208,8 +208,7 @@ function list_load(block, link) {
   request.onreadystatechange = function () {
     if ( request.readyState == 4 && request.status == 200 ) {
       block.innerHTML = request.responseText;
-    	block.querySelector("#left_menu_regions_2").click();
-      block.querySelector("#left_menu_regions_2").focus();
+      try{block.querySelector(".left_menu_select").click();block.querySelector(".left_menu_select").focus()} catch {null}
     }
   };
   request.send( null );

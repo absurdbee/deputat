83d5fb3b5ae68330159d68c710b5b386f1f1de63
Serg <ochkarik1983@mail.ru@>
2021-08-27 17:10:46 +0300

    J

--- generic/mixins.py
+++ generic/mixins.py
@@ -8,9 +8,9 @@ class CategoryListMixin(ContextMixin):
 		context = super(CategoryListMixin,self).get_context_data(**kwargs)
 		context["current_url"] = self.request.path
 		context["federal_elect_list"] = AuthorityList.objects.filter(is_in_left_menu=True)
-		#if self.request.user.is_authenticated:
-		context["user_region"] = self.request.user.city.region
-		context["user_city"] = self.request.user.city
+		if self.request.user.is_authenticated:
+			context["user_region"] = self.request.user.city.region
+			context["user_city"] = self.request.user.city
 		#else:
 		#	context["user_region"] = None
 		#	context["user_city"] = None
--- region/models.py
+++ region/models.py
@@ -26,10 +26,74 @@ class Region(models.Model):
 	def get_elects(self):
 		from elect.models import Elect
 		return Elect.objects.filter(region=self)
+
 	def get_elects_ids(self):
 		from elect.models import Elect
 		elects = Elect.objects.filter(region=self).values("id")
 		return [i['id'] for i in elects]
+
+	def get_senators(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="senat")
+	def is_have_senators(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="senat").exists()
+
+	def get_governors(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="governors")
+	def is_have_governors(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="governors").exists()
+
+	def get_commissioner_child(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="commissioner_child")
+	def is_have_commissioner_child(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="commissioner_child").exists()
+
+	def get_commissioner_human(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="commissioner_human")
+	def is_have_commissioner_human(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="commissioner_human").exists()
+
+	def get_head_hinistry_health(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="head_hinistry_health")
+	def is_have_head_hinistry_health(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="head_hinistry_health").exists()
+
+	def get_chapter_min_enlightenment(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="chapter_min_enlightenment")
+	def is_have_chapter_min_enlightenment(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="chapter_min_enlightenment").exists()
+
+	def get_head_societies_chambers(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="head_societies_chambers")
+	def is_have_head_societies_chambers(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="head_societies_chambers").exists()
+
+	def get_legislative_assembly(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="legislative_assembly")
+	def is_have_legislative_assembly(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="legislative_assembly").exists()
+
+	def get_gos_duma(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="state_duma")
+	def is_have_gos_duma(self):
+		from elect.models import Elect
+		return Elect.objects.filter(region=self, list__slug="state_duma").exists()
 
 	def get_news(self):
 		from blog.models import ElectNew

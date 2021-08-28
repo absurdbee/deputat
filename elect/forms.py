ac5639053efedfddb3188ad769783173f960779a
Serg <ochkarik1983@mail.ru@>
2021-08-28 03:03:42 +0300

    J

--- elect/forms.py
+++ elect/forms.py
@@ -1,13 +1,5 @@
 from django import forms
-from common.model.comments import ElectComment
 from elect.models import Elect
-
-
-class ElectCommentForm(forms.ModelForm):
-	text = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': ''}))
-	class Meta:
-		model = ElectComment
-		fields = ['text']
 
 
 class ElectForm(forms.ModelForm):

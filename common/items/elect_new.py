162fb3bb031e748726801764198bd6902f16522c
Serg <ochkarik1983@mail.ru@>
2021-08-29 15:24:29 +0300

    J

--- common/items/elect_new.py
+++ common/items/elect_new.py
@@ -101,7 +101,7 @@ def get_notify_elect_new(user, notify):
 
 def get_notify_comment_elect_new(user, notify):
     verb = notify.verb
-    if 'LCO' in verb:
+    if 'L' in verb:
         return notify_comment_elect_new(user, notify)
     else:
         return "nnnnnnn"

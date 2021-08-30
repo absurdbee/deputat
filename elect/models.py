d71bb9dbb800e85e6bae6a334c7e83ad91deb934
Serg <ochkarik1983@mail.ru@>
2021-08-30 16:08:03 +0300

    J

--- elect/models.py
+++ elect/models.py
@@ -232,9 +232,9 @@ class Elect(models.Model):
             ElectVotes.objects.create(elect=self, user=user, vote=ElectVotes.LIKE)
             self.like += 1
             self.save(update_fields=['like'])
-            from common.notify.notify import user_notify, user_wall
-            user_notify(user, None, self.pk, "ELE", "u_elec_notify", "LIK", self.pk)
-            user_wall(user, None, self.pk, "ELE", "u_elec_notify", "LIK")
+            #from common.notify.notify import user_notify, user_wall
+            #user_notify(user, None, self.pk, "ELE", "u_elec_notify", "LIK", self.pk)
+            #user_wall(user, None, self.pk, "ELE", "u_elec_notify", "LIK")
         return HttpResponse(json.dumps({"like_count": str(self.likes_count()),"dislike_count": str(self.dislikes_count()),"inert_count": str(self.inerts_count())}),content_type="application/json")
 
     def send_dislike(self, user):
@@ -263,9 +263,9 @@ class Elect(models.Model):
             ElectVotes.objects.create(elect=self, user=user, vote=ElectVotes.DISLIKE)
             self.dislike += 1
             self.save(update_fields=['dislike'])
-            from common.notify.notify import user_notify, user_wall
-            user_notify(user, None, self.pk, "ELE", "u_elec_notify", "DIS", self.pk)
-            user_wall(user, None, self.pk, "ELE", "u_elec_notify", "DIS")
+            #from common.notify.notify import user_notify, user_wall
+            #user_notify(user, None, self.pk, "ELE", "u_elec_notify", "DIS", self.pk)
+            #user_wall(user, None, self.pk, "ELE", "u_elec_notify", "DIS")
         return HttpResponse(json.dumps({"like_count": str(self.likes_count()),"dislike_count": str(self.dislikes_count()),"inert_count": str(self.inerts_count())}),content_type="application/json")
 
     def send_inert(self, user):
@@ -294,9 +294,9 @@ class Elect(models.Model):
             ElectVotes.objects.create(elect=self, user=user, vote=ElectVotes.INERT)
             self.inert += 1
             self.save(update_fields=['inert'])
-            from common.notify.notify import user_notify, user_wall
-            user_notify(user, None, self.pk, "ELE", "u_elec_notify", "INE", self.pk)
-            user_wall(user, None, self.pk, "ELE", "u_elec_notify", "INE")
+            #from common.notify.notify import user_notify, user_wall
+            #user_notify(user, None, self.pk, "ELE", "u_elec_notify", "INE", self.pk)
+            #user_wall(user, None, self.pk, "ELE", "u_elec_notify", "INE")
         return HttpResponse(json.dumps({"like_count": str(self.likes_count()),"dislike_count": str(self.dislikes_count()),"inert_count": str(self.inerts_count())}),content_type="application/json")
 
 

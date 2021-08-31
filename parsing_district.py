04d9d75f3ae954b0971e2873e63060e7a906382c
Serg <ochkarik1983@mail.ru@>
2021-08-31 12:17:25 +0300

    J

--- parsing_district.py
+++ parsing_district.py
@@ -13,27 +13,10 @@ django.setup()
 
 from region.models import Region
 
-response = requests.get(url= "https://election.novayagazeta.ru/api/address/?address=" + "Коми")
-data = response.json()
-print ("кол-во элементов ", len(data))
-print ("кол-во ", len(data[0]))
-print ("полный путь ", data[0][0])
-
-path = data[0][0].split(",")
-
-print ("единица ", path[0])
-if len(path) == 3:
-    print ("дистрикт ", path[1])
-    print ("регион ", path[2])
-else:
-    print ("регион ", path[1])
-
-print ("ссылка ", data[0][2])
-
 for region in Region.objects.all():
     response = requests.get(url= "https://election.novayagazeta.ru/api/address/?address=" + region.name)
     data = response.json()
-    for i in len(data):
+    for i in data:
         print ("кол-во дистриктов/городов", len(data[i]))
         print ("полный путь ", data[i][0])
 

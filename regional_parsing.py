ec6e720693b86f3287f0a6c78e8bf5a5be7b88b1
Serg <ochkarik1983@mail.ru@>
2021-09-01 15:26:11 +0300

    J

--- regional_parsing.py
+++ regional_parsing.py
@@ -18,9 +18,9 @@ city_no_link = 0
 
 for city in City.objects.all():
     if city.link:
-        city_with_link += 0
+        city_with_link += 1
     else:
-        city_no_link += 0
+        city_no_link += 1
 
 print ("Город со ссылкой ", city_with_link)
 print ("Город без ссылки ", city_no_link)

b060fdc6bf367dec4f0a07d20e8e2aa9f2bbafb2
Serg <ochkarik1983@mail.ru@>
2021-09-03 18:40:49 +0300

    J

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -301,7 +301,7 @@ on('body', 'click', '.phone_send', function() {
     if (val == '') {
       return
     } else {
-			parent = _this.parentElement.parentElement.parentElement.parentElement;
+			parent = _this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
 			console.log(parent)
 			if (parent.classList.contains("municipal_authorities")) {
 				url = '/district/elects/'

ddbb7c1467f2139d27bdbb29213fd089d9cee972
Serg <ochkarik1983@mail.ru@>
2021-09-03 18:32:00 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -225,6 +225,7 @@ function cities_list_load(block, link) {
       block.innerHTML = request.responseText;
 
         parent = block.parentElement.parentElement.parentElement.parentElement;
+        console.log(parent);
         if (parent.classList.contains("municipal_authorities")) {
   				city_id = "city_elects_1", district_id = "district_elects_1"
   			}

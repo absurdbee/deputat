63e52d01fb44360309599f7e1daca74f8bd5e55b
Serg <ochkarik1983@mail.ru@>
2021-09-05 14:19:15 +0300

    J

--- region/urls.py
+++ region/urls.py
@@ -3,6 +3,7 @@ from region.views import *
 
 
 urlpatterns = [
+    url(r'^(?P<slug>[\w\-]+)/$', RegionDetailView.as_view(), name='region_detail'),
     url(r'^(?P<slug>[\w\-]+)/$', RegionElectView.as_view(), name='region_index'),
     url(r'^elects/(?P<slug>[\w\-]+)/$', RegionElectDetailView.as_view(), name="region_elects"),
     url(r'^communities/(?P<slug>[\w\-]+)/$', RegionCommunitiesDetailView.as_view(), name="region_communities"),
@@ -15,6 +16,5 @@ urlpatterns = [
     url(r'^load_left_menu_region_get_districts/(?P<slug>[\w\-]+)/$', LoadLeftMenuRegionDistricts.as_view()),
     url(r'^load_districts_for_multiple_form/(?P<pk>\d+)/$', LoadDistrictsMultipleForm.as_view()),
     url(r'^load_region_for_select_regional_elects/$', LoadRegionForSelectRegionalElects.as_view()),
-    url(r'^(?P<slug>[\w\-]+)/$', RegionDetailView.as_view(), name='region_detail'),
     url(r'^load_region_for_select_regional_elects/$', LoadRegionForSelectRegionalElects.as_view()),
 ]

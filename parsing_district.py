fc33db5f035db00b1d0e3763459a1aaa65ce4292
Serg <ochkarik1983@mail.ru@>
2021-09-06 15:19:52 +0300

    J

--- parsing_district.py
+++ parsing_district.py
@@ -15,17 +15,16 @@ django.setup()
 from region.models import Region
 from district.models import District2
 
+spb_oblast_list = ["Бокситогорский муниципальный район,Волосовский муниципальный район,Волховский муниципальный район,Всеволожский муниципальный район,Выборгский муниципальный район,Гатчинский муниципальный район,Кингисеппский муниципальный район,Киришский муниципальный район,Кировский муниципальный район,Лодейнопольский муниципальный район,Ломоносовский муниципальный район,Лужский муниципальный район,Подпорожский муниципальный район,Приозерский муниципальный район,Сланцевский муниципальный район,Сосновоборский городской округ,Тихвинский муниципальный район,Тосненский муниципальный район"]
 
-for region in Region.objects.all():
-    if region.name == "Удмуртская Республика (Удмуртия)":
-        pass
-    elif region.name == "Ханты-Мансийский автономный округ - Югра (Тюменская область)":
-        pass
-    response = requests.get(url= "https://election.novayagazeta.ru/api/address/?address=" + region.name)
+
+region = Region.objects.filter(name="Ленинградская область")
+for i in spb_oblast_list:
+    response = requests.get(url= "https://election.novayagazeta.ru/api/address/?address=" + i)
     data = response.json()
     count = 0
     for i in data:
-        d = District2.objects.create(name=data[count][1], region=region, link=data[count][2])
+        d = District2.objects.create(name=i, region=region, link=data[count][2])
         count += 1
         print ("Добавлен дистрикт ", d.name)
     print ("==============================")

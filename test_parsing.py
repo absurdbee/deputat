72466e7af1704622023d4def51bccbe96275266c
Serg <ochkarik1983@mail.ru@>
2021-09-08 18:42:58 +0300

    J

--- test_parsing.py
+++ test_parsing.py
@@ -39,7 +39,7 @@ def get_page_data(html):
             print("Должность ", item.find("p", class_='fio').text)
             print("Возраст ", deps[1].text)
             print("Образование ", deps[2].text)
-            count += 1
+        count += 1
         print("=================================")
 
 def main():

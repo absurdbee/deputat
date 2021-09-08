a9ba698c1bdf7ff5c23c39391c09dabed78d092a
Serg <ochkarik1983@mail.ru@>
2021-09-08 14:53:32 +0300

    J

--- elect/models.py
+++ elect/models.py
@@ -545,7 +545,7 @@ class Elect(models.Model):
             return '<span class="elect_rating_icon"><span class="integer">0</span><svg fill="#FFEB84" enable-background="new 0 0 20 20" width="24" height="24" viewBox="0 0 24 24"><g><rect x="0"></rect><polygon points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"></polygon></g></svg></span>'
         else:
             from django.db.models import Avg
-            
+
             query = ElectRating.objects.filter(elect_id=self.id)
             vakcine = query.aggregate(Avg('vakcine'))['vakcine__avg']
             pp_825 = query.aggregate(Avg('pp_825'))['pp_825__avg']
@@ -576,7 +576,7 @@ class Elect(models.Model):
                 color = "#83C77D"
             elif avg == 5.0:
                 color = "#63BE7B"
-            return '<span class="elect_rating_icon"><span class="integer">', str(avg), '</span><svg fill="', color, '" enable-background="new 0 0 20 20" width="24" height="24" viewBox="0 0 24 24"><g><rect x="0"></rect><polygon points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"></polygon></g></svg></span>'
+            return '<span class="elect_rating_icon"><span class="integer">' + str(avg) + '</span><svg fill="' + color + '" enable-background="new 0 0 20 20" width="24" height="24" viewBox="0 0 24 24"><g><rect x="0"></rect><polygon points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"></polygon></g></svg></span>'
 
 
 class LinkElect(models.Model):

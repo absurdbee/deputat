a176e8ad5e73ed872643e8fd40d7fad03d5a79a8
Serg <ochkarik1983@mail.ru@>
2021-09-08 14:50:36 +0300

    J

--- elect/models.py
+++ elect/models.py
@@ -574,7 +574,7 @@ class Elect(models.Model):
                 color = "#83C77D"
             elif avg == 5.0:
                 color = "#63BE7B"
-            return return '<span class="elect_rating_icon"><span class="integer">', str(avg), '</span><svg fill="', color, '" enable-background="new 0 0 20 20" width="24" height="24" viewBox="0 0 24 24"><g><rect x="0"></rect><polygon points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"></polygon></g></svg></span>'
+            return '<span class="elect_rating_icon"><span class="integer">', str(avg), '</span><svg fill="', color, '" enable-background="new 0 0 20 20" width="24" height="24" viewBox="0 0 24 24"><g><rect x="0"></rect><polygon points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"></polygon></g></svg></span>'
 
 
 class LinkElect(models.Model):

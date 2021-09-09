08a29036f9be7666fe9deac23c4c60b6597eae74
Serg <ochkarik1983@mail.ru@>
2021-09-09 18:32:31 +0300

    J

--- common/items/elect_new.py
+++ common/items/elect_new.py
@@ -98,8 +98,11 @@ def get_notify_elect_new(user, notify):
     verb = notify.verb
     if verb == "ELNC":
         from blog.models import ElectNew
-        new = ElectNew.objects.get(pk=notify.object_id)
-        return ''.join(['<div class="" data-pk="', str(notify.object_id), '"><div class="media"><figure>•</figure><div class="media-body pl-1"><p class="mb-0 small">Ваша новость <span class="elect_new_window pointer underline" style="font-weight: bold;">', new.title, '</span> прошла проверку модератора и опубликована. Благодарим.</p><p class="mb-0 small_2">', notify.get_created(), '</p></div></div></div>'])
+        try:
+            new = ElectNew.objects.get(pk=notify.object_id)
+            return ''.join(['<div class="" data-pk="', str(notify.object_id), '"><div class="media"><figure>•</figure><div class="media-body pl-1"><p class="mb-0 small">Ваша новость <span class="elect_new_window pointer underline" style="font-weight: bold;">', new.title, '</span> прошла проверку модератора и опубликована. Благодарим.</p><p class="mb-0 small_2">', notify.get_created(), '</p></div></div></div>'])
+        except:
+            return ''
     elif 'LIK' in verb or 'DIS' in verb or 'INE' in verb:
         return notify_elect_new(user, notify)
     elif verb == "ITE":

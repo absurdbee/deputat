485007e52dcaf1da2114e9f8808d3ef3b722a058
Serg <ochkarik1983@mail.ru@>
2021-09-10 16:01:06 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -239,7 +239,10 @@ on('body', 'click', '.ajax', function(event) {
   var url = this.getAttribute('href');
   if (url != window.location.pathname){
     ajax_get_reload(url)
-  } else {toast_info("Вы уже на этой странице")};
+  } else {
+		mobile_menu_close();
+		toast_info("Вы уже на этой странице")
+	};
 })
 
 

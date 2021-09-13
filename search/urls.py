c6f50d64ffb945c91f732d027819cc5c729f5921
Serg <ochkarik1983@mail.ru@>
2021-09-13 18:38:16 +0300

    J

--- search/urls.py
+++ search/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', SearchView.as_view(), name='search'),
     url(r'^elect_filter/', AllElectSearch.as_view()),
     url(r'^elect_add_elect_new_filter/', ElectAddElectNewSearch.as_view()),
+    url(r'^tags_filter/(?P<pk>\d+)/', TagsSearch.as_view()),
 ]

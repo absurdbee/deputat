6f331343245760b4ecdfd8c5fbff683cc9ae09bc
Serg <ochkarik1983@mail.ru@>
2021-09-14 18:53:59 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -662,8 +662,10 @@ function scrolled(_block) {
 };
 
 on('body', 'click', '.toggle_fixed_block', function() {
-  document.body.querySelector(".notify_dropdown").style.display = "none";
-  document.body.querySelector(".get_user_notify_box").classList.remove("show");
+  try{
+    document.body.querySelector(".notify_dropdown").style.display = "none";
+    document.body.querySelector(".get_user_notify_box").classList.remove("show")
+  } catch { null }
 });
 on('body', 'click', '.main-menu', function() {
   document.body.querySelector(".notify_dropdown").style.display = "none";

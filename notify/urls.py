4702a6ce1b6ceeef3493ebd0d9bc371e29b9dfad
Serg <ochkarik1983@mail.ru@>
2021-09-17 13:44:05 +0300

    J

--- notify/urls.py
+++ notify/urls.py
@@ -4,5 +4,6 @@ from notify.views import *
 urlpatterns=[
 	url(r'^$', AllNotifyView.as_view(), name='all_notify'),
 	url(r'^recent/$', RecentNotifyView.as_view()),
+	url(r'^all_read/$', AllReadNotifyView.as_view()),
 	url(r'^new_wall/(?P<pk>\d+)/$', NewWall.as_view()),
 ]

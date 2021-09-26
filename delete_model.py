5a445e03e34972e73b0ab1b8dcd0dd2dd2eeff57
Serg <ochkarik1983@mail.ru@>
2021-09-26 12:04:06 +0300

    J

--- delete_model.py
+++ delete_model.py
@@ -22,8 +22,8 @@ candidate_list = AuthorityList.objects.get(slug="candidate_duma")
 
 lists = Q(list__slug="candidate_duma")|Q(list__slug="state_duma")
 
-for elect in Elect.objects.filter(list=candidate_list):
-    if elect.old and elect.list.all().count() > 1:
+for elect in Elect.objects.filter(list=deputat_list):
+    if elect.list.all().count() > 1:
         count += 1
         print("прогон ", count)
-        print(elect, " Старый")
+        #print(elect, " Старый")

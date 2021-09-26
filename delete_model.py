3381240b0bae0afe743766d4557514b661923d58
Serg <ochkarik1983@mail.ru@>
2021-09-26 11:36:45 +0300

    J

--- delete_model.py
+++ delete_model.py
@@ -12,18 +12,38 @@ import django
 django.setup()
 
 from elect.models import Elect
+from blog.models import ElectNew
 from lists.models import AuthorityList
 from django.db.models import Q
 
+deputat_list = AuthorityList.objects.get(slug="state_duma")
+candidate_list = AuthorityList.objects.get(slug="candidate_duma")
 
 count = 0
-lists =  Q(list__slug="candidate_duma")|Q(list__slug="state_duma")
-for elect in Elect.objects.all():
+lists = Q(list__slug="candidate_duma")|Q(list__slug="state_duma")
 
+for elect in Elect.objects.all():
     if Elect.objects.filter(lists, name=elect.name).values("pk").count() > 1:
         count += 1
         print("---------- Прогон ", count, "-----------")
-        for i in Elect.objects.filter(name=elect.name):
-            print("Имя: " , i.name,  " | Возраст: " , i.birthday,  " | Список: " , i.get_first_list())
+
+        #берем обе копии депутата - одна в госдуме, вторая - новый кандидат
         elects = Elect.objects.filter(lists, name=elect.name)
-        
+        current_elect = elects.objects.get(list=deputat_list)
+        delete_elect = elects.objects.get(list=candidate_list)
+
+        # депутату из думы присваиваем новый список, ведь он теперь и депутат
+        current_elect.list.add(candidate_list)
+
+        # а это депутат, которого будем удалять, но сначала нам надо найти все активности с ним связанные,
+        # и записать их на старого депутата, чтобы они не потерялись
+
+        news = ElectNew.objects.filter(elect=delete_elect)
+        for new in news:
+            new.elect = current_elect
+            new.save(update_fields=["elect"])
+            print("Активность получила нового депутата!")
+
+        delete_elect.old = True
+        delete_elect.save(update_fields=["old"])
+        print("Копия помечена!")

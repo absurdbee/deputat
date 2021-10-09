acd3cc4e94e1bf03f352e520f8d889a69fa14198
Serg <ochkarik1983@mail.ru@>
2021-10-09 16:58:29 +0300

    J

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from main.views import *
 
 urlpatterns = [
 	url(r'^$', MainPageView.as_view(), name="main"),
-	url(r'^main_region/(?P<pk>\d+)/$', MainRegionView.as_view()),
+	url(r'^main_region/(?P<uuid>[0-9a-f-]+)/$', MainRegionView.as_view()),
 	url(r'^main_map/$', MainMapView.as_view()),
 	url(r'^main_stat/$', MainStatView.as_view()),
 	url(r'^main_media/$', MainMediaView.as_view()),

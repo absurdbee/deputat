46524f1f294871f9807840006fedee54d9beb31a
Serg <ochkarik1983@mail.ru@>
2021-11-08 18:30:23 +0300

    J

--- terms/views.py
+++ terms/views.py
@@ -1,11 +1,11 @@
 from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
-from common.templates import get_small_template
+from common.templates import get_full_template
 
 
 class TermsView(TemplateView, CategoryListMixin):
     template_name = None
 
     def get(self,request,*args,**kwargs):
-        self.template_name = get_small_template("terms/terms.html", request.user, request.META['HTTP_USER_AGENT'])
+        self.template_name = get_full_template("terms/terms.html", request.user, request.META['HTTP_USER_AGENT'])
         return super(TermsView,self).get(request,*args,**kwargs)

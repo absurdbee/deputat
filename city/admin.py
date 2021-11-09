c7b404516deb37e025dcbaed3ececfefb140cefb
Serg <ochkarik1983@mail.ru@>
2021-11-09 18:04:13 +0300

    J

--- city/admin.py
+++ city/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from city.models import City
 
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'order', 'pk']
+    search_fields = ['title']
+    class Meta:
+            model = City
+
+admin.site.register(City, CityAdmin)

0cbec852edc1fece47784c9d9b101f32b44277e3
Serg <ochkarik1983@mail.ru@>
2021-11-11 14:05:54 +0300

    J

--- logs/model/manage_elect_new.py
+++ logs/model/manage_elect_new.py
@@ -31,7 +31,7 @@ class ElectNewManageLog(models.Model):
             if "I" in self.action_type:
                 from blog.models import ElectNew
                 new = ElectNew.objects.get(pk=self.item)
-                return '<span data-pk="' + str(new.pk) + '"><span><span><span class="elect_new_window pointer underline" style="font-weight: bold;">' +  new.title + '</span></span></span></span>'
+                return '<span data-pk="' + str(new.pk) + '"><span><span><span><span class="elect_new_window pointer underline" style="font-weight: bold;">' +  new.title + '</span></span></span></span></span>'
             else:
                 from common.model.comments import ElectNewComment
                 comment = ElectNewComment.objects.get(pk=self.item)

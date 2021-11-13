c891ebbfbb0fb0908648f81d721a72fb8be832fc
Serg <ochkarik1983@mail.ru@>
2021-11-13 16:34:18 +0300

    J

--- users/models.py
+++ users/models.py
@@ -234,7 +234,7 @@ class User(AbstractUser):
 
     def get_news(self):
         from blog.models import ElectNew
-        query = Q(creator_id=self.pk, type="PUB", community__isnull=True)
+        query = Q(Q(creator_id=self.pk)|Q(community__isnull=True)) & Q(Q(type="PUB")|Q(type="SUG")|Q(type="REJ"))
         query.add(Q(elect_id__in=self.get_elect_subscribers_ids(), type="PUB"), Q.AND)
         return ElectNew.objects.filter(query)
     def get_my_news(self):

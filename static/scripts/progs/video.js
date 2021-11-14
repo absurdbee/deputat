a22119df704792a96bb32bda9fea11f2caf03d52
Serg <ochkarik1983@mail.ru@>
2021-11-14 17:59:19 +0300

    H

--- static/scripts/progs/video.js
+++ static/scripts/progs/video.js
@@ -5,6 +5,13 @@ on('body', 'click', '.remove_video_from_media_list_collection', function() {
   mob_send_change(this, "/video/user_progs/remove_video_from_media_list/", "add_video_in_media_list_collection", "Добавить в медиа-список")
 });
 
+on('body', 'click', '.add_video_in_media_list_collection_2', function() {
+  item_send_change(this, "/video/user_progs/add_video_in_media_list/", "remove_video_from_media_list_collection_2", "Убрать из медиа-списка")
+});
+on('body', 'click', '.remove_video_from_media_list_collection_2', function() {
+  item_send_change(this, "/video/user_progs/remove_video_from_media_list/", "add_video_in_media_list_collection_2", "Добавить в медиа-список")
+});
+
 on('body', 'click', '.u_video_list_add', function() {
   create_fullscreen("/video/user_progs/add_list/", "worker_fullscreen")
 });

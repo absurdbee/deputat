09976241fecfdf2e4c394a98c9284cd7b2496af8
Serg <ochkarik1983@mail.ru@>
2021-11-21 10:31:55 +0300

    J

--- static/scripts/functions/general.js
+++ static/scripts/functions/general.js
@@ -1149,12 +1149,6 @@ function send_comment(form, block, link, prepend) {
     link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     link_.open('POST', link, true);
     link_.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    if (!form.querySelector(".text-comment").value && !form.querySelector(".comment_attach_block").firstChild){
-      toast_error("Напишите или прикрепите что-нибудь");
-      form.querySelector(".text-comment").style.border = "1px #FF0000 solid";
-      form.querySelector(".dropdown").style.border = "1px #FF0000 solid";
-      return
-    };
     link_.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             form.querySelector(".text-comment").value = "";

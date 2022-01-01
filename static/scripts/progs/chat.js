956ec16d61aa147c4bf0b33b27811f85043784d5
Serg <ochkarik1983@mail.ru@>
2022-01-01 15:57:15 +0300

    J

--- static/scripts/progs/chat.js
+++ static/scripts/progs/chat.js
@@ -598,7 +598,7 @@ function send_message (form_post, url) {
   if (form_post.querySelector("#audio") && CURRENT_BLOB) {
     form_data = new FormData(form_post);
     form_data.append("voice", CURRENT_BLOB, 'fileName.wav');
-    formData.append("text", " ");
+    form_data.append("text", " ");
     console.log(CURRENT_BLOB);
   }
   else {

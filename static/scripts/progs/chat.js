2f3c4f143c96c54a6e3ae9c71a895a75c193bf1a
Serg <ochkarik1983@mail.ru@>
2022-01-10 14:18:50 +0300

    J

--- static/scripts/progs/chat.js
+++ static/scripts/progs/chat.js
@@ -3,11 +3,8 @@ CURRENT_BLOB = null;
 on('body', 'mouseover', '.dropdown', function() {
   this.querySelector(".dropdown-menu").classList.add("show")
 });
-on('body', 'mouseout', '.dropdown-menu', function() {
-  this.classList.remove("show")
-});
-on('body', 'mouseenter', '.dropdown-menu', function() {
-  this.classList.add("show")
+on('body', 'mouseout', '.dropdown', function() {
+  this.querySelector(".dropdown-menu").classList.remove("show")
 });
 
 on('#ajax', 'click', '.m_select_photo', function() {
